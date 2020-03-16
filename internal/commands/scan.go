2df628aee851b655889e2a0d5cd30272ca896c70
Daniel Amsellem <dannyboy412@gmail.com>
2020-03-16 19:15:08 +0200

    Input edge cases

--- internal/commands/scan.go
+++ internal/commands/scan.go
@@ -1,7 +1,11 @@
 package commands
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+
+	"github.com/checkmarxDev/scans/pkg/scans"
 
 	http "github.com/checkmarxDev/ast-cli/internal/http"
 	"github.com/spf13/cobra"
@@ -20,9 +24,33 @@ func NewScanCommand(endpoint string) *cobra.Command {
 		Use:   "create",
 		Short: "Creates and runs a new scan",
 		Run: func(cmd *cobra.Command, args []string) {
+			var input []byte
+			var err error
 			if scanInputFile != "" {
+				// Reading from input file
 				fmt.Printf("Reading input from file %s\n", scanInputFile)
+				input, err = ioutil.ReadFile(scanInputFile)
+				if err != nil {
+					fmt.Printf("Failed opening input file: %s\n", err.Error())
+					return
+				}
+			} else if scanInput != "" {
+				// Reading from standard input
+				input = bytes.NewBufferString(scanInput).Bytes()
+			} else {
+				// No input was given
+				fmt.Println("Failed creating a scan: no input was given")
+				return
 			}
+			var scanModel *scans.ScanResponseModel
+			var errorModel *scans.ErrorModel
+			scanModel, errorModel, err = scansWrapper.Create(input)
+			if err != nil {
+				fmt.Printf("Failed ceating a scan: %s", err.Error())
+				return
+			}
+			fmt.Println(scanModel)
+			fmt.Println(errorModel)
 		},
 	}
 	createScanCmd.Flags().StringVarP(&scanInput, "input", "i", "", "The object representing the requested scan, in JSON format")
