fafad007796032f72fb6560dc156163eddf0d90d
Daniel Amsellem <dannyboy412@gmail.com>
2020-03-16 18:46:43 +0200

    Create commands and flags

--- internal/commands/scan.go
+++ internal/commands/scan.go
@@ -13,15 +13,37 @@ func NewScanCommand(endpoint string) *cobra.Command {
 		Short: "Manage AST scans",
 	}
 	scansWrapper := http.NewHTTPScansWrapper(endpoint)
+
+	var scanInput string
+	var scanInputFile string
 	createScanCmd := &cobra.Command{
 		Use:   "create",
 		Short: "Creates and runs a new scan",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Create scan running")
-			fmt.Println(scansWrapper)
+			if scanInputFile != "" {
+				fmt.Printf("Reading input from file %s\n", scanInputFile)
+			}
 		},
 	}
-	scanCmd.AddCommand(createScanCmd)
+	createScanCmd.Flags().StringVarP(&scanInput, "input", "i", "", "The object representing the requested scan, in JSON format")
+	createScanCmd.Flags().StringVarP(&scanInputFile, "inputFile", "f", "", "A file holding the requested scan object in JSON format. Takes precedence over --input")
+
+	getScanCmd := &cobra.Command{
+		Use:   "get",
+		Short: "Returns information about a scan",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Get scan running")
+		},
+	}
+	deleteScanCmd := &cobra.Command{
+		Use:   "delete",
+		Short: "Stops a scan from running",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Delete scan running")
+		},
+	}
+	scanCmd.AddCommand(createScanCmd, getScanCmd, deleteScanCmd)
+	fmt.Println(scansWrapper)
 	return scanCmd
 }
 
