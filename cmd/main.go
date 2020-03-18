c680bb5e845af7cd55d5851885653d7fb02c9ce5
Daniel Amsellem <dannyboy412@gmail.com>
2020-03-18 21:30:13 +0200

    Added default values for env vars

--- cmd/main.go
+++ cmd/main.go
@@ -9,15 +9,16 @@ import (
 )
 
 const (
-	astEp              = "AST_ENDPOINT"
-	astEpDisplay       = "AST endpoint"
+	astSchema          = "AST_SCHEMA"
+	astHost            = "AST_HOST"
+	astPort            = "80"
 	scansPath          = "SCANS_PATH"
 	projectsPath       = "PROJECTS_PATH"
 	resultsPath        = "RESULTS_PATH"
 	uploadsPath        = "UPLOADS_PATH"
 	logLevel           = "CLI_LOG_LEVEL"
 	successfulExitCode = 0
-	failureExitCode    = 1
+	// failureExitCode    = 1
 )
 
 func main() {
@@ -27,16 +28,20 @@ func main() {
 	viper.SetConfigType("env")
 	_ = viper.ReadInConfig()
 
+	viper.SetDefault(astSchema, "http")
+	viper.SetDefault(astHost, "localhost")
+	viper.SetDefault(astPort, "80")
 	viper.SetDefault(scansPath, "scans")
 	viper.SetDefault(projectsPath, "projects")
 	viper.SetDefault(uploadsPath, "uploads")
 	viper.SetDefault(resultsPath, "results")
 	viper.SetDefault(logLevel, "DEBUG")
 
-	ast := viper.GetString(astEp)
-	if ast == "" {
-		requiredErrAndExit(astEp, astEpDisplay)
-	}
+	schema := viper.GetString(astSchema)
+	host := viper.GetString(astHost)
+	port := viper.GetString(astPort)
+	ast := fmt.Sprintf("%s://%s:%s/api", schema, host, port)
+
 	scans := viper.GetString(scansPath)
 	projects := viper.GetString(projectsPath)
 	uploads := viper.GetString(uploadsPath)
@@ -52,12 +57,3 @@ func main() {
 // env GOOS=windows GOARCH=amd64 go build -o ./bin/ast.exe ./cmd
 // "bin/ast.exe" scan create  --sources c:\CODE\ast\example\sources.zip  --inputFile  ./payloads/uploads.json
 // "bin/ast.exe" scan get  --id 4d9a9189-ddcc-4aa0-ba2f-9d6d7f92eceb
-
-func errorAndExit(msg string) {
-	fmt.Println(msg)
-	os.Exit(failureExitCode)
-}
-
-func requiredErrAndExit(key, displayName string) {
-	errorAndExit(fmt.Sprintf("%s is required. Please specify it using the environment variable %s or in a config.env file.", displayName, key))
-}
