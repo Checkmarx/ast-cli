f1c0aa11dac5afdd4eb497ad4159567172a9f9da
Daniel Amsellem <dannyboy412@gmail.com>
2020-03-23 13:02:19 +0200

    Added the tags command for scan

--- internal/wrappers/scans-http.go
+++ internal/wrappers/scans-http.go
@@ -12,6 +12,7 @@ import (
 
 const (
 	failedToParseGetAll = "Failed to parse get-all response"
+	failedToParseTags   = "Failed to parse tags response"
 )
 
 type ScansHTTPWrapper struct {
@@ -77,6 +78,35 @@ func (s *ScansHTTPWrapper) Delete(scanID string) (*scansModels.ScanResponseModel
 		return nil, nil, err
 	}
 	return handleResponse(resp, err, http.StatusOK)
+}
+
+func (s *ScansHTTPWrapper) Tags() (*[]string, *scansModels.ErrorModel, error) {
+	resp, err := http.Get(s.url + "/tags")
+	if err != nil {
+		return nil, nil, err
+	}
+	decoder := json.NewDecoder(resp.Body)
+
+	defer resp.Body.Close()
+	switch resp.StatusCode {
+	case http.StatusBadRequest, http.StatusInternalServerError:
+		errorModel := scansModels.ErrorModel{}
+		err = decoder.Decode(&errorModel)
+		if err != nil {
+			return nil, nil, errors.Wrapf(err, failedToParseTags)
+		}
+		return nil, &errorModel, nil
+	case http.StatusOK:
+		tags := []string{}
+		err = decoder.Decode(&tags)
+		if err != nil {
+			return nil, nil, errors.Wrapf(err, failedToParseTags)
+		}
+		return &tags, nil, nil
+
+	default:
+		return nil, nil, errors.Errorf("Unknown response status code %d", resp.StatusCode)
+	}
 }
 
 func NewHTTPScansWrapper(url string) ScansWrapper {
