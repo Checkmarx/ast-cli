12426d651b901b9e34c7deca452dc8e9165c71c8
ronenl2 <ronen.levinson@checkmarx.com>
2020-04-13 21:09:43 +0300

    Add cluster command

---
+++ internal/commands/cluster.go
@@ -0,0 +1,39 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func NewClusterCommand() *cobra.Command {
+	clusterCmd := &cobra.Command{
+		Use:   "cluster",
+		Short: "Manage AST cluster",
+	}
+
+	// installClusterCmd := &cobra.Command{
+	// 	Use:   "install",
+	// 	Short: "Install a new cluster",
+	// 	RunE:  runInstallClusterCommand(projectsWrapper),
+	// }
+
+	// lsClusterCmd := &cobra.Command{
+	// 	Use:   "ls",
+	// 	Short: "Returns cluster information",
+	// 	RunE:  runLsClusterCommand(projectsWrapper),
+	// }
+
+	deployClusterCmd := &cobra.Command{
+		Use:   "deploy",
+		Short: "Deploy AST resources",
+		RunE:  runDeployClusterCommand(),
+	}
+
+	clusterCmd.AddCommand(deployClusterCmd)
+	return clusterCmd
+}
+func runDeployClusterCommand() func(cmd *cobra.Command, args []string) error {
+	fmt.Println("deploy cluster")
+	return nil
+}
