371ffb75fe72706031775dc486d23142b1ef61fc
Igor Altman <Igor.Altman@checkmarx.com>
2020-05-06 09:38:28 +0300

    added sr stats parameter validation

--- internal/commands/rm_test.go
+++ internal/commands/rm_test.go
@@ -3,47 +3,58 @@ package commands
 import (
 	"testing"
 
+	"gotest.tools/assert/cmp"
+
 	"gotest.tools/assert"
 )
 
 func TestSastResourcesScansCommand(t *testing.T) {
-	cmd := createASTTestCommand()
-	err := executeTestCommand(cmd, "sast-resources", "scans", "--format", "pretty")
+	err := executeASTTestCommand("sast-resources", "scans", "--format", "pretty")
 	assert.NilError(t, err)
-	err = executeTestCommand(cmd, "sr", "scans")
+	err = executeASTTestCommand("sr", "scans")
 	assert.NilError(t, err)
-	err = executeTestCommand(cmd, "sr", "scans", "--format", "table")
+	err = executeASTTestCommand("sr", "scans", "--format", "table")
 	assert.NilError(t, err)
-	err = executeTestCommand(cmd, "sast-resources", "scans", "-h")
+	err = executeASTTestCommand("sast-resources", "scans", "-h")
 	assert.NilError(t, err)
 }
 
 func TestSastResourcesEnginesCommand(t *testing.T) {
-	cmd := createASTTestCommand()
-	err := executeTestCommand(cmd, "sast-resources", "engines", "--format", "table")
+	err := executeASTTestCommand("sast-resources", "engines", "--format", "table")
 	assert.NilError(t, err)
-	err = executeTestCommand(cmd, "sr", "engines")
+	err = executeASTTestCommand("sr", "engines")
 	assert.NilError(t, err)
-	err = executeTestCommand(cmd, "sr", "engines", "--format", "pretty")
+	err = executeASTTestCommand("sr", "engines", "--format", "pretty")
 	assert.NilError(t, err)
-	err = executeTestCommand(cmd, "sr", "engines", "-h")
+	err = executeASTTestCommand("sr", "engines", "-h")
 	assert.NilError(t, err)
 }
 
 func TestSastResourcesStatsCommand(t *testing.T) {
+	err := executeASTTestCommand("sr", "stats", "-v", "-r", "hour", "-m", "engine-running")
+	assert.NilError(t, err)
+	err = executeASTTestCommand("sr", "stats", "-v", "--resolution", "hour", "-m", "scan-running")
+	assert.NilError(t, err)
+	err = executeASTTestCommand("sr", "stats", "-v", "-r", "day", "--metric", "scan-pending")
+	assert.NilError(t, err)
+	err = executeASTTestCommand("sr", "stats", "-v", "--resolution", "week")
+	assert.NilError(t, err)
+	err = executeASTTestCommand("sr", "stats", "-v", "--metric", "engine-waiting")
+	assert.NilError(t, err)
+	err = executeASTTestCommand("sast-resources", "-v", "stats", "--format", "table")
+	assert.NilError(t, err)
+	err = executeASTTestCommand("sr", "stats", "-v", "--format", "pretty")
+	assert.NilError(t, err)
+	err = executeASTTestCommand("sr", "stats", "-h")
+	assert.NilError(t, err)
+	err = executeASTTestCommand("sr", "stats", "-r", "day", "-m", "lala")
+	assert.Assert(t, cmp.Equal(err.Error(), "unknown metric lala"))
+
+	err = executeASTTestCommand("sr", "stats", "-r", "sdfsd", "-m", "engine-waiting")
+	assert.Assert(t, cmp.Equal(err.Error(), "unknown resolution sdfsd"))
+}
+
+func executeASTTestCommand(args ...string) error {
 	cmd := createASTTestCommand()
-	err := executeTestCommand(cmd, "sast-resources", "stats", "--format", "table")
-	assert.NilError(t, err)
-	err = executeTestCommand(cmd, "sr", "stats", "--format", "pretty")
-	assert.NilError(t, err)
-	err = executeTestCommand(cmd, "sr", "stats", "-h")
-	assert.NilError(t, err)
-	err = executeTestCommand(cmd, "sr", "stats", "resolution", "hour", "m", "running")
-	assert.NilError(t, err)
-	err = executeTestCommand(cmd, "sr", "stats", "r", "day", "metric", "scan-queued")
-	assert.NilError(t, err)
-	err = executeTestCommand(cmd, "sr", "stats", "resolution", "week")
-	assert.NilError(t, err)
-	err = executeTestCommand(cmd, "sr", "stats", "metric", "engine", "engine-waiting")
-	assert.NilError(t, err)
+	return executeTestCommand(cmd, args...)
 }
