732fc0aa8cea9d2f297df9f6efe839afcf976dbd
Igor Altman <Igor.Altman@checkmarx.com>
2020-05-10 18:59:57 +0300

    moment resolution

--- internal/wrappers/rm.go
+++ internal/wrappers/rm.go
@@ -3,6 +3,7 @@ package wrappers
 import rm "github.com/checkmarxDev/sast-rm/pkg/api/v1/rest"
 
 var StatResolutions = map[string]StatResolution{
+	"moment": StatResolutionMoment,
 	"minute": StatResolutionMinute,
 	"hour":   StatResolutionHour,
 	"day":    StatResolutionDay,
@@ -12,6 +13,7 @@ var StatResolutions = map[string]StatResolution{
 type StatResolution string
 
 const (
+	StatResolutionMoment = "moment"
 	StatResolutionMinute = "minute"
 	StatResolutionHour   = "hour"
 	StatResolutionDay    = "day"
