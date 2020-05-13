e2e40080062f63b869c408c69321faeb04402f03
Daniel Amsellem <daniel.amsellem@checkmarx.com>
2020-05-13 13:49:51 +0300

    Logging from STDERR. Need to return the buffer and write it to the log file. WIP

--- internal/commands/aio.go
+++ internal/commands/aio.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -35,12 +36,20 @@ func NewAIOCommand(scriptsWrapper wrappers.ScriptsWrapper) *cobra.Command {
 		RunE:  runInstallAIOCommand(scriptsWrapper),
 	}
 
+	startAIOCmd := &cobra.Command{
+		Use:   "start",
+		Short: "Start All-In-One AST",
+		RunE:  runStartAIOCommand(scriptsWrapper),
+	}
+
 	installAIOCmd.PersistentFlags().String(logFileFlag, "./install.ast.log",
 		"Installation log file path (optional)")
 	installAIOCmd.PersistentFlags().String(configFileFlag, "",
-		"Configuration file path to provide to the AIO installation (optional)")
+		"Configuration file path to provide to the AST installation (optional)")
+	startAIOCmd.PersistentFlags().String(configFileFlag, "",
+		"Configuration file path for AST (optional)")
 
-	aioCmd.AddCommand(installAIOCmd)
+	aioCmd.AddCommand(installAIOCmd, startAIOCmd)
 	return aioCmd
 }
 
@@ -58,21 +67,31 @@ func runInstallAIOCommand(scriptsWrapper wrappers.ScriptsWrapper) func(cmd *cobr
 		})
 		logrus.RegisterExitHandler(closeLogFile(logFile))
 
-		var installCmdOutput []byte
+		installCmdStdOutputBuffer := bytes.NewBufferString("")
+		installCmdStdErrorBuffer := bytes.NewBufferString("")
 		var upCmdOutput []byte
 
 		installScriptPath := scriptsWrapper.GetInstallScriptPath()
-		writeInstallationInfoToConsoleAndLog("AIO installation started")
+		writeToInstallationLog("AIO installation started")
+		fmt.Println("AIO installation started")
 		writeToInstallationLog("Running installation script from path", installScriptPath)
-		installCmdOutput, err = runBashCommand(installScriptPath)
+		err = runBashCommand(installScriptPath, installCmdStdOutputBuffer, installCmdStdErrorBuffer)
 		if err != nil {
 			msg := fmt.Sprintf("%s: Failed to run install command", failedInstallingAIO)
 			logrus.WithFields(logrus.Fields{
 				"err": err,
 			}).Println(msg)
+			writeToInstallationLog("OUTPUT FROM INSTALL COMMAND START")
+			writeToInstallationLog(installCmdStdOutputBuffer.String())
+			writeToInstallationLog("OUTPUT FROM INSTALL COMMAND END")
+			writeToInstallationLog("ERROR FROM INSTALL COMMAND START")
+			writeToInstallationLog(installCmdStdErrorBuffer.String())
+			writeToInstallationLog(err.Error())
+			writeToInstallationLog("ERROR FROM INSTALL COMMAND END")
+
 			return errors.Wrapf(err, msg)
 		}
-		writeToInstallationLog(string(installCmdOutput))
+		writeToInstallationLog(installCmdStdOutputBuffer.String())
 
 		// Run the up command after installation
 		upCmdOutput, err = up(cmd, scriptsWrapper)
@@ -81,10 +100,26 @@ func runInstallAIOCommand(scriptsWrapper wrappers.ScriptsWrapper) func(cmd *cobr
 			logrus.WithFields(logrus.Fields{
 				"err": err,
 			}).Println(msg)
+			writeToInstallationLog(string(upCmdOutput))
 			return errors.Wrapf(err, msg)
 		}
 		writeToInstallationLog(string(upCmdOutput))
-		writeInstallationInfoToConsoleAndLog("AIO installation completed successfully")
+		writeToInstallationLog("AIO installation completed successfully")
+		fmt.Println("AIO installation completed successfully")
+		return nil
+	}
+}
+
+func runStartAIOCommand(scriptsWrapper wrappers.ScriptsWrapper) func(cmd *cobra.Command, args []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		// Run the up command after installation
+		upCmdOutput, err := up(cmd, scriptsWrapper)
+		if err != nil {
+			msg := fmt.Sprintf("Failed to start AST")
+			fmt.Println(string(upCmdOutput))
+			return errors.Wrapf(err, msg)
+		}
+		fmt.Println("AIO started successfully")
 		return nil
 	}
 }
@@ -122,18 +157,14 @@ func up(cmd *cobra.Command, scriptsWrapper wrappers.ScriptsWrapper) ([]byte, err
 	logAgeDays := fmt.Sprintf("log_rotation_age_days=%s", configuration.Log.Rotation.MaxAgeDays)
 	privateKeyFile := fmt.Sprintf("tls_private_key_file=%s", configuration.Network.PrivateKeyFile)
 	certificateFile := fmt.Sprintf("tls_certificate_file=%s", configuration.Network.CertificateFile)
-
-	cmdOutput, err = runBashCommand(upScriptPath,
+	upCmdStdOutputBuffer := bytes.NewBufferString("")
+	upCmdStdErrorBuffer := bytes.NewBufferString("")
+	err = runBashCommand(upScriptPath, upCmdStdOutputBuffer, upCmdStdErrorBuffer,
 		logMaxSize, logAgeDays, privateKeyFile, certificateFile)
 	if err != nil {
-		return cmdOutput, errors.Wrapf(err, "%s: Failed to run up command", failedRunningAIO)
+		return cmdOutput, errors.Wrapf(err, "Failed to run up command")
 	}
 	return cmdOutput, nil
-}
-
-func writeInstallationInfoToConsoleAndLog(msg ...string) {
-	fmt.Println(msg)
-	writeToInstallationLog(msg...)
 }
 
 func writeToInstallationLog(msg ...string) {
