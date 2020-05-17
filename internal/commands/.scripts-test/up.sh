893be48e48a2b1a1e8d0780f7bfcc57287271632
Daniel Amsellem <daniel.amsellem@checkmarx.com>
2020-05-17 21:31:54 +0300

    Added fqdn and deploy_TLS

--- internal/commands/.scripts-test/up.sh
+++ internal/commands/.scripts-test/up.sh
@@ -8,8 +8,10 @@ do
             log_rotation_age_days)    log_rotation_age_days=${VALUE} ;;
             private_key_path)    private_key_path=${VALUE} ;;
             certificate_path)    certificate_path=${VALUE} ;;
+            fqdn)    fqdn=${VALUE} ;;
           deploy_DB)    deploy_DB=${VALUE} ;;
+          deploy_TLS)    deploy_TLS=${VALUE} ;;
             *)
     esac
 done
-echo $log_rotation_size#$log_rotation_age_days#$private_key_path#$certificate_path#$(( deploy_DB == true ))
+echo $log_rotation_size#$log_rotation_age_days#$private_key_path#$certificate_path#$fqdn#$(( deploy_DB == true ))#$(( deploy_TLS == true ))
