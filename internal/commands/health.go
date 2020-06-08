ccd92a4fdb66abc5e3a333f139bac6ce745008c6
DavidCohen <davidc@checkmarx.com>
2020-06-08 19:58:27 +0300

    Add db http health check and mock Change health checks to run concurrently

--- internal/commands/health.go
+++ internal/commands/health.go
@@ -2,8 +2,7 @@ package commands
 
 import (
 	"fmt"
-
-	"github.com/pkg/errors"
+	"sync"
 
 	"github.com/checkmarxDev/ast-cli/internal/wrappers"
 	"github.com/spf13/cobra"
@@ -13,22 +12,37 @@ func NewHealthCheckCommand(healthCheckWrapper wrappers.HealthCheckWrapper) *cobr
 	return &cobra.Command{
 		Use:   "health-check",
 		Short: "Run AST health check",
-		RunE:  runAllHealthChecks(healthCheckWrapper),
+		Run:   runAllHealthChecks(healthCheckWrapper),
 	}
 }
 
-func runWebAppHealthCheck(healthCheckWrapper wrappers.HealthCheckWrapper) error {
-	status, err := healthCheckWrapper.RunWebAppCheck()
+func runHealthCheck(serviceName string, checker wrappers.HealthChecker) {
+	status, err := checker()
 	if err != nil {
-		return errors.Wrapf(err, "Web App error")
+		fmt.Printf("%v error %v\n", serviceName, err)
+	} else {
+		fmt.Printf("%v status: %v\n", serviceName, status)
 	}
-
-	fmt.Printf("Web App status: %v", status)
-	return nil
 }
 
-func runAllHealthChecks(healthCheckWrapper wrappers.HealthCheckWrapper) func(cmd *cobra.Command, args []string) error {
-	return func(cmd *cobra.Command, args []string) error {
-		return runWebAppHealthCheck(healthCheckWrapper)
+func runChecksConcurrently(checks map[string]wrappers.HealthChecker) {
+	var wg sync.WaitGroup
+	for serviceName, checker := range checks {
+		wg.Add(1)
+		go func(n string, c wrappers.HealthChecker) {
+			defer wg.Done()
+			runHealthCheck(n, c)
+		}(serviceName, checker)
+	}
+
+	wg.Wait()
+}
+
+func runAllHealthChecks(healthCheckWrapper wrappers.HealthCheckWrapper) func(cmd *cobra.Command, args []string) {
+	return func(cmd *cobra.Command, args []string) {
+		runChecksConcurrently(map[string]wrappers.HealthChecker{
+			"Web App": healthCheckWrapper.RunWebAppCheck,
+			"DB":      healthCheckWrapper.RunDBCheck,
+		})
 	}
 }
