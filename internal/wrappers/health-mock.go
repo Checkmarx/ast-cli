b954e85037541dcd05ec3a9924119c90ef767a86
DavidCohen <davidc@checkmarx.com>
2020-06-11 14:01:53 +0300

    Ad import healthcheckModel from healthcheck service and use it instead of HealthStatus

--- internal/wrappers/health-mock.go
+++ internal/wrappers/health-mock.go
@@ -1,12 +1,24 @@
 package wrappers
 
+import healthcheckApi "github.com/checkmarxDev/healthcheck/api/rest/v1"
+
 type HealthCheckMockWrapper struct {
 }
 
 func (h *HealthCheckMockWrapper) RunWebAppCheck() (*HealthStatus, error) {
-	return &HealthStatus{Success: true, Message: ""}, nil
+	return &HealthStatus{
+		&healthcheckApi.HealthcheckModel{
+			Success: true,
+			Message: "",
+		},
+	}, nil
 }
 
 func (h *HealthCheckMockWrapper) RunDBCheck() (*HealthStatus, error) {
-	return &HealthStatus{Success: true, Message: ""}, nil
+	return &HealthStatus{
+		&healthcheckApi.HealthcheckModel{
+			Success: true,
+			Message: "",
+		},
+	}, nil
 }
