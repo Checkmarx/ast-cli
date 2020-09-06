9b90ab2039a9b4c2c9c525ba1536a02d354780eb
DavidCohen <davidc@checkmarx.com>
2020-09-06 12:48:33 +0300

    Fix integration tests Change scan delete command to support muliple ids

--- internal/commands/query_test.go
+++ internal/commands/query_test.go
@@ -42,7 +42,7 @@ func TestRunCLoneCommand(t *testing.T) {
 	err := executeTestCommand(cmd, "-v", "query", "clone", "mock")
 	assert.NilError(t, err)
 	wd, _ := os.Getwd()
-	mockFilePath := filepath.Join(wd, queriesRepoDestFileName)
+	mockFilePath := filepath.Join(wd, QueriesRepoDestFileName)
 	mockRepoFile, err := os.Open(mockFilePath)
 	assert.NilError(t, err, "failed to open repo mock file")
 	defer os.Remove(mockFilePath)
