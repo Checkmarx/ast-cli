d2a0ae8db9c735ccfcf57b1933e36e9150d22f46
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-01-26 18:11:13 -0800

    Another integration test change.

--- internal/commands/.scripts/integration_up.sh
+++ internal/commands/.scripts/integration_up.sh
@@ -6,9 +6,6 @@ echo $AST_ACCESS_KEY_ID
 echo $BASE_URI
 echo $AST_ACCESS_KEY_SECRET
 echo "Done setting keys, trigginer process"
-sudo export BASE_URI="http://ast.cxflow-ci.com" && \
-export AST_ACCESS_KEY_ID="ast-plugins-3d1b8499-b3cf-43cf-8425-0bb212ca44d3" && \
-export AST_ACCESS_KEY_SECRET="c710a92f-d103-4bc5-89ce-6d138f976b9e" && \
 go test ./test/integration/scan_test.go \
             ./test/integration/root_test.go \
             ./test/integration/project_test.go \
