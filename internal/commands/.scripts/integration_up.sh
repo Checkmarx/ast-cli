d771cff7d8e7c3d3708f9fe3f15dcfb0bdcd1384
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-01-27 09:48:41 -0800

    Updated integration test script.

--- internal/commands/.scripts/integration_up.sh
+++ internal/commands/.scripts/integration_up.sh
@@ -2,7 +2,7 @@ echo "Authenticating to AST test server."
 #./ast.exe auth register -u XXX -p XXX > auth.txt
 # export $(awk 'NR==1 { print }' auth.txt | awk -F'=' '{ print "export AST_ACCESS_KEY_ID==" $2 }')
 # export $(awk 'NR==2 { print }' auth.txt | awk -F'=' '{ print "AST_ACCESS_KEY_SECRET==" $2 }')
-# export BASE_URI="http://ast.cxflow-ci.com"
+export BASE_URI="http://ast.cxflow-ci.com"
 # export BASE_URI="http://127.0.0.1:8080"
 # export AST_ACCESS_KEY_ID="ast-plugins-3d1b8499-b3cf-43cf-8425-0bb212ca44d3"
 # export AST_ACCESS_KEY_SECRET="c710a92f-d103-4bc5-89ce-6d138f976b9e"
