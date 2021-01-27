d2b29c8875858bbe8bcd280b63c4d8dfc016fb5d
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-01-26 17:50:44 -0800

    Adjusted environment setup script

--- internal/commands/.scripts/integration_up.sh
+++ internal/commands/.scripts/integration_up.sh
@@ -1,3 +1,8 @@
 echo "Setting auth keys"
+export BASE_URI="http://ast.cxflow-ci.com"
 export AST_ACCESS_KEY_ID="ast-plugins-3d1b8499-b3cf-43cf-8425-0bb212ca44d3"
 export AST_ACCESS_KEY_SECRET="c710a92f-d103-4bc5-89ce-6d138f976b9e"
+echo $AST_ACCESS_KEY_ID
+echo $BASE_URI
+echo AST_ACCESS_KEY_SECRET
+echo "Done setting keys"
