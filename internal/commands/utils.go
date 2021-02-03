fc9da86053ad2aa964dec029101019c528aca1cb
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-02-02 23:28:40 -0800

    Updated utils unit tests.

--- internal/commands/utils.go
+++ internal/commands/utils.go
@@ -1,10 +1,7 @@
 package commands
 
 import (
-	"fmt"
-
 	"github.com/checkmarxDev/ast-cli/internal/wrappers"
-	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
@@ -27,14 +24,4 @@ func NewUtilsCommand(healthCheckWrapper wrappers.HealthCheckWrapper,
 	configCmd := NewConfigCommand()
 	scanCmd.AddCommand(healthCheckCmd, ssiCmd, rmCmd, queriesCmd, logsCmd, configCmd)
 	return scanCmd
-}
-
-func runTestCommand() func(cmd *cobra.Command, args []string) error {
-	return func(cmd *cobra.Command, args []string) error {
-		if 1 == 2 {
-			return errors.Errorf("%s: Please provide a scan ID", "message")
-		}
-		fmt.Println("Print a message")
-		return nil
-	}
 }
