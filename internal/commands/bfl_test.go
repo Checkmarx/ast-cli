30eced127b1c2365b85c1f0f4fc33cb02939d8fc
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-03-22 16:15:57 -0700

    Feature/testing fixes (#170)
    * Enabled auth testing.
    
    * Enabled BFL tests.
    
    * Enabled some of the project management tests.
    
    * Updated project tests.
    
    * Enabled more project tests.
    
    * Tweaking project tests.
    
    * More project tweaks.
    
    * Tweak to scan commands.
    
    * Scan test tweaks.
    
    * Made project scan create return 0 instead of one when project name isn't
    found.
    
    * Tweaked project tests.

--- internal/commands/bfl_test.go
+++ internal/commands/bfl_test.go
@@ -2,40 +2,37 @@
 
 package commands
 
-/* Renable
+import (
+	"testing"
+
+	"gotest.tools/assert"
+)
+
 func TestBFLHelp(t *testing.T) {
 	cmd := createASTTestCommand()
 	err := executeTestCommand(cmd, "help", "bfl")
 	assert.NilError(t, err)
 }
-*/
 
-/* Renable
 func TestRunGetBFLByScanIDCommandNoScanID(t *testing.T) {
 	cmd := createASTTestCommand()
 	err := executeTestCommand(cmd, "-v", "bfl")
 	assert.Assert(t, err != nil)
 	assert.Assert(t, err.Error() == "Failed getting BFL: Please provide a scan ID")
 }
-*/
 
-/* Renable
 func TestRunGetBFLByScanIDCommand(t *testing.T) {
 	cmd := createASTTestCommand()
 	err := executeTestCommand(cmd, "-v", "bfl", "list", "MOCK")
 	assert.NilError(t, err)
 }
-*/
 
-/* Renable
 func TestRunGetBFLByScanIDCommandPretty(t *testing.T) {
 	cmd := createASTTestCommand()
 	err := executeTestCommand(cmd, "-v", "bfl", "--format", "list", "list", "MOCK")
 	assert.NilError(t, err)
 }
-*/
 
-/* Renable
 func TestRunGetBFLByScanIDCommandFilters(t *testing.T) {
 	cmd := createASTTestCommand()
 	err := executeTestCommand(cmd, "-v", "bfl", "--format", "table", "list", "MOCK", "--filter")
@@ -50,4 +47,3 @@ func TestRunGetBFLByScanIDCommandFilters(t *testing.T) {
 	err = executeTestCommand(cmd, "-v", "bfl", "--format", "list", "list", "MOCK", "--filter", "a=b")
 	assert.NilError(t, err)
 }
-*/
