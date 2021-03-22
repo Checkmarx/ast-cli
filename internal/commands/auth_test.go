ca6488ccf6952a42709f99ef4122c991b832e3b2
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-03-22 10:58:43 -0700

    Enabled auth testing.

--- internal/commands/auth_test.go
+++ internal/commands/auth_test.go
@@ -2,23 +2,24 @@
 
 package commands
 
-/* Renable
+import (
+	"testing"
+
+	"gotest.tools/assert"
+)
+
 func TestAuthHelp(t *testing.T) {
 	cmd := createASTTestCommand()
 	err := executeTestCommand(cmd, "help", "auth")
 	assert.NilError(t, err)
 }
-*/
 
-/* Renable
 func TestAuthNoSub(t *testing.T) {
 	cmd := createASTTestCommand()
 	err := executeTestCommand(cmd, "auth")
 	assert.NilError(t, err)
 }
-*/
 
-/* Renable
 func TestRunCreateOath2ClientCommand(t *testing.T) {
 	cmd := createASTTestCommand()
 	err := executeTestCommand(cmd, "-v", "auth", "register", "--username", "username",
@@ -29,12 +30,9 @@ func TestRunCreateOath2ClientCommand(t *testing.T) {
 		"-p", "password", "--roles", "admin,user")
 	assert.NilError(t, err)
 }
-*/
 
-/* Renable
 func TestRunCreateOath2ClientCommandInvalid(t *testing.T) {
 	cmd := createASTTestCommand()
 	err := executeTestCommand(cmd, "-v", "auth", "register")
 	assert.Assert(t, err != nil)
 }
-*/
