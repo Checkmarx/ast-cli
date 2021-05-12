3abd44fbfe029ef6d9af336b8e86729a348605bd
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-05-12 12:04:20 -0700

    Many small tweaks to CLI parameters:
    - (--incremental) should be (--sast-incremental)
    - Scan preset is included with all json (scan create) requests. The default is provided as "Checkmarx Default" if it isn't specified with (--sast-preset-name) option.
    - Removed (--configure) from (utils) but its still available from the root level
    - (Auth register) help was incorrectly show "ast" as the command.
    - (--agent) help name says "Scan origin name"
    - (--auth-path), has been removed until we hear otherwise.
    - The client ID help is now "The oAuth2 client ID"
    - The client ID secret help is now "The oAuth2 client secret"
    - Moved (completion) option under (utils)

--- internal/commands/utils.go
+++ internal/commands/utils.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"os"
+
 	"github.com/checkmarxDev/ast-cli/internal/wrappers"
 	"github.com/spf13/cobra"
 )
@@ -21,7 +23,67 @@ func NewUtilsCommand(healthCheckWrapper wrappers.HealthCheckWrapper,
 	rmCmd := NewSastResourcesCommand(rmWrapper)
 	queriesCmd := NewQueryCommand(queriesWrapper, uploadsWrapper)
 	logsCmd := NewLogsCommand(logsWrapper)
-	configCmd := NewConfigCommand()
-	scanCmd.AddCommand(healthCheckCmd, ssiCmd, rmCmd, queriesCmd, logsCmd, configCmd)
+	//
+	/// Complete command
+	//
+	var completionCmd = &cobra.Command{
+		Use:   "completion [bash|zsh|fish|powershell]",
+		Short: "Generate completion script",
+		Long: `To load completions:
+	
+	Bash:
+	
+		$ source <(cx completion bash)
+	
+		# To load completions for each session, execute once:
+		# Linux:
+		$ cx completion bash > /etc/bash_completion.d/cx
+		# macOS:
+		$ cx completion bash > /usr/local/etc/bash_completion.d/cx
+	
+	Zsh:
+	
+		# If shell completion is not already enabled in your environment,
+		# you will need to enable it.  You can execute the following once:
+	
+		$ echo "autoload -U compinit; compinit" >> ~/.zshrc
+	
+		# To load completions for each session, execute once:
+		$ cx completion zsh > "${fpath[1]}/_cx"
+	
+		# You will need to start a new shell for this setup to take effect.
+	
+	fish:
+	
+		$ cx completion fish | source
+	
+		# To load completions for each session, execute once:
+		$ cx completion fish > ~/.config/fish/completions/cx.fish
+	
+	PowerShell:
+	
+		PS> cx completion powershell | Out-String | Invoke-Expression
+	
+		# To load completions for every new session, run:
+		PS> cx completion powershell > cx.ps1
+		# and source this file from your PowerShell profile.
+	`,
+		DisableFlagsInUseLine: true,
+		ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+		Args:                  cobra.ExactValidArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			switch args[0] {
+			case "bash":
+				cmd.Root().GenBashCompletion(os.Stdout)
+			case "zsh":
+				cmd.Root().GenZshCompletion(os.Stdout)
+			case "fish":
+				cmd.Root().GenFishCompletion(os.Stdout, true)
+			case "powershell":
+				cmd.Root().GenPowerShellCompletion(os.Stdout)
+			}
+		},
+	}
+	scanCmd.AddCommand(healthCheckCmd, ssiCmd, rmCmd, queriesCmd, logsCmd, completionCmd)
 	return scanCmd
 }
