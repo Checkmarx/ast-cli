f09e2b0f5abf5df6a144c6543c66917719e49463
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-05-13 21:28:52 -0700

    -Denial_Of_Service_Resource_Exhaustion in configuration.go line 123

--- internal/wrappers/client.go
+++ internal/wrappers/client.go
@@ -218,7 +218,7 @@ func enrichWithPasswordCredentials(request *http.Request, username, password,
 			"failed to authenticate")
 	}
 
-	//request.Header.Add("Authorization", "Bearer "+*accessToken)
+	request.Header.Add("Authorization", "Bearer "+*accessToken)
 	return request, nil
 }
 
