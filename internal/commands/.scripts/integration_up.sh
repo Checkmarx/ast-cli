51c46fdfcde479c1d03640c0337c9ab89e07c17a
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2021-06-02 19:33:13 +0100

    fix some tests

--- internal/commands/.scripts/integration_up.sh
+++ internal/commands/.scripts/integration_up.sh
@@ -4,4 +4,4 @@ go test ./test/integration/scan_test.go \
             ./test/integration/health_check_test.go \
             ./test/integration/result_test.go \
             ./test/integration/query_test.go \
-            ./test/integration/sast_resources_test.go -v
+            ./test/integration/sast_resources_test.go
