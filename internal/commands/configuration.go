c0da23fd2e7b760a7e23d19b65eda092b1d7f699
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-06-21 15:36:59 -0700

    Feature/branch (#211)
    - Add (--branch) tag and documentation to CLI (scan create).
    - Some incorrect commands weren't displaying the expected help messages. This happened when the incorrect command was added to the end of a correctly formatted command, like this (scan create junkCommad). This should be an error but the command would have run correctly simply ignoring the junk. This first occured when a user tried to do this (scan create help) and found no help was displayed. The help command is incorrect but it should have shown an error. This behavor has been resolved and help is now displayed whever bad commands are entered.
    - Added command (utils env) that displays the detected CX environment variables.
    - Fixed (--nowait) environment message spelling error
    - Correct help grammar for (scan cancel).
    - removed (--repo-url) from (project create).
    - Added missing env.go source file.
    - Added (auth client-validate) to verify configure user credentials, or credentials passed on the CLI. This does a simple (scan list) command to verify communication is working, there may be a better enpoint to verify against?
    - (auth register) now display a clearer error message when user enters bad credentials
    -Added (--tags) option to (scan create). Tags will be added to scans. The tags can be added one at a time or as a list like this, ex: "tag1,tag2,etc".
    - Reverted all integration test changes.
    - renamed validate-client command to validate.
    - Modified printTable so that it always filters "Updated at" column on all tables. The "Created At" column has also been truciated to only inclue the date.
    - Modified printList so that it always filters "Updated at" attribute. The "Created At" column has also been truciated to only include the date.
    - Updating linting issues.
    - Removed "-w" shortcut option from --nowait
    - The (--tags) option now accepts key:value pairs, exame: foo:42,tesla:motor,noValue,etc
    - (auth register) wasn't accepting (--tenant) CLI option, this has been
      fixed.
    - (configure set) wasn't storing values. This was caused by a fix to the
      help system in the last revision. The has been fixed and the
      (configure set) command now accepts (--prop-name and --prop-value)
      options for better compatibility with other CLI commands.
    - Added correct mock code for tests.
    - Updated mock code.
    - (auth register) wasn't accepting (--tenant) CLI option, this has been fixed.
    - (configure set) wasn't storing values. This was caused by a fix to the help system in the last revision. The has been fixed and the (configure set) command now accepts (--prop-name and --prop-value) options for better compatibility with other CLI commands.

--- internal/commands/configuration.go
+++ internal/commands/configuration.go
@@ -9,6 +9,8 @@ import (
 
 const (
 	failedSettingProp = "Failed to set property"
+	propNameFlag      = "prop-name"
+	propValFlag       = "prop-value"
 )
 
 func NewConfigCommand() *cobra.Command {
@@ -33,28 +35,28 @@ func NewConfigCommand() *cobra.Command {
 		Short: "Set configuration property (cx_base_uri, cx_base_auth_uri, cx_apikey, cx_tenant, cx_client_id, cx_client_secret, cx_http_proxy)",
 		RunE:  runSetValue(),
 	}
+	setCmd.PersistentFlags().String(propNameFlag, "", "Name of property set")
+	setCmd.PersistentFlags().String(propValFlag, "", "Value of property set")
 	scanCmd.AddCommand(storValCmd, setCmd)
 	return scanCmd
 }
 
 func runSetValue() func(cmd *cobra.Command, args []string) error {
 	return func(cmd *cobra.Command, args []string) error {
-		const minArgCnt = 2
-		if len(args) < minArgCnt {
-			return errors.Errorf("%s: Please provide a property to set and a value", failedDeleting)
-		}
-		if args[0] == params.AstAPIKey {
-			wrappers.SetConfigProperty(args[0], args[1])
-		} else if args[0] == params.BaseURIKey {
-			wrappers.SetConfigProperty(args[0], args[1])
-		} else if args[0] == params.BaseAuthURIKey {
-			wrappers.SetConfigProperty(args[0], args[1])
-		} else if args[0] == params.ProxyKey {
-			wrappers.SetConfigProperty(args[0], args[1])
-		} else if args[0] == params.AccessKeySecretConfigKey {
-			wrappers.SetConfigProperty(args[0], args[1])
-		} else if args[0] == params.AccessKeyIDConfigKey {
-			wrappers.SetConfigProperty(args[0], args[1])
+		propName, _ := cmd.Flags().GetString(propNameFlag)
+		propValue, _ := cmd.Flags().GetString(propValFlag)
+		if propName == params.AstAPIKey {
+			wrappers.SetConfigProperty(propName, propValue)
+		} else if propName == params.BaseURIKey {
+			wrappers.SetConfigProperty(propName, propValue)
+		} else if propName == params.BaseAuthURIKey {
+			wrappers.SetConfigProperty(propName, propValue)
+		} else if propName == params.ProxyKey {
+			wrappers.SetConfigProperty(propName, propValue)
+		} else if propName == params.AccessKeySecretConfigKey {
+			wrappers.SetConfigProperty(propName, propValue)
+		} else if propName == params.AccessKeyIDConfigKey {
+			wrappers.SetConfigProperty(propName, propValue)
 		} else {
 			return errors.Errorf("%s: unknown property or bad value", failedSettingProp)
 		}
