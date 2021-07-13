47719c0ddd5bdc23a4b649614f8f2f1329ebe497
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-07-12 20:12:38 -0700

    - Cleaned ntlm proxy messages.

--- internal/wrappers/proxy-ntml.go
+++ internal/wrappers/proxy-ntml.go
@@ -138,7 +138,6 @@ func NewNTLMProxyDialContext(dialer *net.Dialer, proxyURL *url.URL,
 	}
 	return func(ctx context.Context, network, addr string) (net.Conn, error) {
 		dialProxy := func() (net.Conn, error) {
-			fmt.Printf("ntlm> Will connect to proxy at " + proxyURL.Host)
 			if proxyURL.Scheme == "https" {
 				return tls.DialWithDialer(dialer, "tcp", proxyURL.Host, tlsConfig)
 			}
@@ -161,16 +160,15 @@ func WrapDialContext(dialContext DialContext, proxyAddress, proxyUsername, proxy
 func dialAndNegotiate(addr, proxyUsername, proxyPassword, proxyDomain string, baseDial func() (net.Conn, error)) (net.Conn, error) {
 	conn, err := baseDial()
 	if err != nil {
-		fmt.Printf("ntlm> Could not call dial context with proxy: %s", err)
+		fmt.Printf("Could not call dial context with proxy: %s", err)
 		return conn, err
 	}
 	// NTLM Step 1: Send Negotiate Message
 	negotiateMessage, err := newNegotiateMessage(proxyDomain, "")
 	if err != nil {
-		fmt.Printf("ntlm> Could not negotiate domain '%s': %s", proxyDomain, err)
+		fmt.Printf("Could not negotiate domain '%s': %s", proxyDomain, err)
 		return conn, err
 	}
-	fmt.Printf("ntlm> NTLM negotiate message: '%s'", base64.StdEncoding.EncodeToString(negotiateMessage))
 	header := make(http.Header)
 	header.Set("Proxy-Authorization", fmt.Sprintf("NTLM %s", base64.StdEncoding.EncodeToString(negotiateMessage)))
 	header.Set("Proxy-Connection", "Keep-Alive")
@@ -182,46 +180,42 @@ func dialAndNegotiate(addr, proxyUsername, proxyPassword, proxyDomain string, ba
 	}
 	err = connect.Write(conn)
 	if err != nil {
-		fmt.Printf("ntlm> Could not write negotiate message to proxy: %s", err)
+		fmt.Printf("Could not write negotiate message to proxy: %s", err)
 		return conn, err
 	}
-	fmt.Printf("ntlm> Successfully sent negotiate message to proxy")
 	// NTLM Step 2: Receive Challenge Message
 	br := bufio.NewReader(conn)
 	resp, err := http.ReadResponse(br, connect)
 	if err != nil {
-		fmt.Printf("ntlm> Could not read response from proxy: %s", err)
+		fmt.Printf("Could not read response from proxy: %s", err)
 		return conn, err
 	}
 	_, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Printf("ntlm> Could not read response body from proxy: %s", err)
+		fmt.Printf("Could not read response body from proxy: %s", err)
 		return conn, err
 	}
 	resp.Body.Close()
 	if resp.StatusCode != http.StatusProxyAuthRequired {
-		fmt.Printf("ntlm> Expected %d as return status, got: %d", http.StatusProxyAuthRequired, resp.StatusCode)
+		fmt.Printf("Expected %d as return status, got: %d", http.StatusProxyAuthRequired, resp.StatusCode)
 		return conn, errors.New(http.StatusText(resp.StatusCode))
 	}
 	challenge := strings.Split(resp.Header.Get("Proxy-Authenticate"), " ")
 	if len(challenge) < ntmlChallengeLen {
-		fmt.Printf("ntlm> The proxy did not return an NTLM challenge, got: '%s'", resp.Header.Get("Proxy-Authenticate"))
+		fmt.Printf("The proxy did not return an NTLM challenge, got: '%s'", resp.Header.Get("Proxy-Authenticate"))
 		return conn, errors.New("no NTLM challenge received")
 	}
-	fmt.Printf("ntlm> NTLM challenge: '%s'", challenge[1])
 	challengeMessage, err := base64.StdEncoding.DecodeString(challenge[1])
 	if err != nil {
-		fmt.Printf("ntlm> Could not base64 decode the NTLM challenge: %s", err)
+		fmt.Printf("Could not base64 decode the NTLM challenge: %s", err)
 		return conn, err
 	}
 	// NTLM Step 3: Send Authorization Message
-	fmt.Printf("ntlm> Processing NTLM challenge with username '%s' and password with length %d", proxyUsername, len(proxyPassword))
 	authenticateMessage, err := processChallenge(challengeMessage, proxyUsername, proxyPassword)
 	if err != nil {
-		fmt.Printf("ntlm> Could not process the NTLM challenge: %s", err)
+		fmt.Printf("Could not process the NTLM challenge: %s", err)
 		return conn, err
 	}
-	fmt.Printf("ntlm> NTLM authorization: '%s'", base64.StdEncoding.EncodeToString(authenticateMessage))
 	header.Set("Proxy-Authorization", fmt.Sprintf("NTLM %s", base64.StdEncoding.EncodeToString(authenticateMessage)))
 	connect = &http.Request{
 		Method: "CONNECT",
@@ -230,22 +224,21 @@ func dialAndNegotiate(addr, proxyUsername, proxyPassword, proxyDomain string, ba
 		Header: header,
 	}
 	if err = connect.Write(conn); err != nil {
-		fmt.Printf("ntlm> Could not write authorization to proxy: %s", err)
+		fmt.Printf("Could not write authorization to proxy: %s", err)
 		return conn, err
 	}
 	resp, err = http.ReadResponse(br, connect)
 	if err != nil {
-		fmt.Printf("ntlm> Could not read response from proxy: %s", err)
+		fmt.Printf("Could not read response from proxy: %s", err)
 		resp.Body.Close()
 		return conn, err
 	}
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("ntlm> Expected %d as return status, got: %d", http.StatusOK, resp.StatusCode)
+		fmt.Printf("Expected %d as return status, got: %d", http.StatusOK, resp.StatusCode)
 		resp.Body.Close()
 		return conn, errors.New(http.StatusText(resp.StatusCode))
 	}
 	// Succussfully authorized with NTLM
-	fmt.Printf("ntlm> Successfully injected NTLM to connection")
 	resp.Body.Close()
 	return conn, nil
 }
