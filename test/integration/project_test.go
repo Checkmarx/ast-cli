b1deb5ee33dd2b66cfc943084fc4e3514edc76a4
Diogo Pimenta <diogo.pimenta@checkmarx.com>
2021-07-13 09:53:12 +0100

    Project integration test - Add step for getting the project with 'show' command - Add test case for asserting failure when creating a project with the same name as an already existing project

--- test/integration/project_test.go
+++ test/integration/project_test.go
@@ -19,20 +19,40 @@ import (
 func TestProjectsE2E(t *testing.T) {
 	projectID := createProject(t)
 
-	response := getProjectsByID(t, projectID)
+	response := listProjectByID(t, projectID)
 
 	assert.Equal(t, len(response), 1, "Total projects should be 1")
 	assert.Equal(t, response[0].ID, projectID, "Project ID should match the created project")
 
+	assert.Equal(t, showProject(t, projectID).ID, projectID, "Project ID should match the created project")
+
 	deleteProject(t, projectID)
 
-	response = getProjectsByID(t, projectID)
+	response = listProjectByID(t, projectID)
 
 	assert.Equal(t, len(response), 0, "Total projects should be 0 as the project was deleted")
 }
 
+// Create the same project twice and assert that it fails
+func TestCreateAlreadyExisting(t *testing.T) {
+	projectName := fmt.Sprintf("integration_test_project_%s", uuid.New().String())
+
+	createProjCommand, outBuffer := createRedirectedTestCommand(t)
+
+	err := execute(createProjCommand, "project", "create", "--format", "json", "--project-name", projectName)
+	assert.NilError(t, err, "Creating a project should pass")
+
+	createdProject := projectsRESTApi.ProjectResponseModel{}
+	_ = unmarshall(t, outBuffer, &createdProject, "Reading project create response JSON should pass")
+
+	defer deleteProject(t, createdProject.ID)
+
+	err = execute(createProjCommand, "project", "create", "--format", "json", "--project-name", projectName)
+	assert.Assert(t, err != nil, "Creating a project with the same name should fail.")
+}
+
 func createProject(t *testing.T) string {
-	projectName := fmt.Sprintf("integration_test_%s", uuid.New().String())
+	projectName := fmt.Sprintf("integration_test_project_%s", uuid.New().String())
 	createProjCommand, outBuffer := createRedirectedTestCommand(t)
 
 	err := execute(createProjCommand, "project", "create", "--format", "json", "--project-name", projectName)
@@ -53,7 +73,7 @@ func deleteProject(t *testing.T, projectID string) {
 	assert.NilError(t, err, "Deleting a project should pass")
 }
 
-func getProjectsByID(t *testing.T, projectID string) []projectsRESTApi.ProjectResponseModel {
+func listProjectByID(t *testing.T, projectID string) []projectsRESTApi.ProjectResponseModel {
 	idFilter := fmt.Sprintf("ids=%s", projectID)
 	getAllCommand, outputBuffer := createRedirectedTestCommand(t)
 
@@ -64,4 +84,17 @@ func getProjectsByID(t *testing.T, projectID string) []projectsRESTApi.ProjectRe
 	_ = unmarshall(t, outputBuffer, &projects, "Reading all projects response JSON should pass")
 
 	return projects
+}
+
+func showProject(t *testing.T, projectID string) projectsRESTApi.ProjectResponseModel {
+
+	showCommand, outputBuffer := createRedirectedTestCommand(t)
+
+	err := execute(showCommand, "project", "show", "--format", "json", "--project-id", projectID)
+	assert.NilError(t, err, "Getting the project should pass")
+
+	var project projectsRESTApi.ProjectResponseModel
+	_ = unmarshall(t, outputBuffer, &project, "Reading project JSON should pass")
+
+	return project
 }
