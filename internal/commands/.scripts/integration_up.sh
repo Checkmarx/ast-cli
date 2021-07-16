f97cbba9db8d4576de652622798eb45ebe60a931
Diogo Pimenta <diogo.pimenta@checkmarx.com>
2021-07-16 10:09:06 +0100

    Remove call to removed test file

--- internal/commands/.scripts/integration_up.sh
+++ internal/commands/.scripts/integration_up.sh
@@ -3,5 +3,4 @@ go test ./test/integration/scan_test.go \
             ./test/integration/project_test.go \
             ./test/integration/health_check_test.go \
             ./test/integration/result_test.go \
-            ./test/integration/query_test.go \
-            ./test/integration/sast_resources_test.go
+            ./test/integration/query_test.go
