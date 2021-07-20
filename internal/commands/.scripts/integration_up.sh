e9fb937a710887f7817067371d7861bcfa001824
Diogo Pimenta <diogo.pimenta@checkmarx.com>
2021-07-20 11:25:20 +0100

    Add launch of a squid instance to the integration testing script

--- internal/commands/.scripts/integration_up.sh
+++ internal/commands/.scripts/integration_up.sh
@@ -1,1 +1,3 @@
+docker run --name squid -d -p 3128:3128 -v ./internal/commands/.scripts/squid.conf:/etc/squid/squid.conf datadog/squid
+
 go test -tags integration -v github.com/checkmarxDev/ast-cli/test/integration
