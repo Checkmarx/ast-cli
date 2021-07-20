09ed98680cc56a2b6125082ad5701f5524f9f4c6
Diogo Pimenta <diogo.pimenta@checkmarx.com>
2021-07-20 11:35:35 +0100

    Mount passwords file and use pwd to build absolute directory in circleCI

--- internal/commands/.scripts/integration_up.sh
+++ internal/commands/.scripts/integration_up.sh
@@ -1,3 +1,9 @@
-docker run --name squid -d -p 3128:3128 -v ./internal/commands/.scripts/squid.conf:/etc/squid/squid.conf datadog/squid
+docker run \
+  --name squid \
+  -d \
+  -p 3128:3128 \
+  -v $(pwd)/internal/commands/.scripts/squid.conf:/etc/squid/squid.conf \
+  -v $(pwd)/internal/commands/.scripts/passwords:/etc/squid/passwords \
+  datadog/squid
 
 go test -tags integration -v github.com/checkmarxDev/ast-cli/test/integration
