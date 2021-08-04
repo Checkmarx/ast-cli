a03cae6d51c268724df7e50f95fee534543c1680
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-08-04 09:30:29 -0700

    Feature/sarif (#241)
    * Sarif support for (result list), adds new format flag 'sarif'. This has currently been tested with mocked data and updates are required when real data is available.
    * Added integration tests for (result list) format type JSON and Sarif.
    * Added comment about missing data in sarif processing

--- internal/commands/result_test.go
+++ internal/commands/result_test.go
@@ -13,3 +13,15 @@ func TestResultHelp(t *testing.T) {
 	err := executeTestCommand(cmd, "help", "result")
 	assert.NilError(t, err)
 }
+
+func TestRunGetResultsByScanIdSarifFormat(t *testing.T) {
+	cmd := createASTTestCommand()
+	err := executeTestCommand(cmd, "-v", "result", "list", "--scan-id", "MOCK", "--format", "sarif")
+	assert.NilError(t, err)
+}
+
+func TestRunGetResultsByScanIdJsonFormat(t *testing.T) {
+	cmd := createASTTestCommand()
+	err := executeTestCommand(cmd, "-v", "result", "list", "--scan-id", "MOCK", "--format", "json")
+	assert.NilError(t, err)
+}
