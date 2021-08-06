8df50d5f948d026d9c4f8aa8d4462ef5d58c947f
Diogo Pimenta <diogo.pimenta@checkmarx.com>
2021-08-06 09:59:29 +0100

    Shorten mock data so no linting errors are thrown and it's only a single line

--- internal/wrappers/results-http.go
+++ internal/wrappers/results-http.go
@@ -273,14 +273,7 @@ var mockResults = `
           "status": "RECURRENT",
           "state": "CONFIRMED",          
           "data": {
-            "description": "Apache Commons BeanUtils, as 
-						distributed in lib/commons-beanutils-1.8.0.jar in Apache Struts 1.x 
-						through 1.3.10 and in other products requiring commons-beanutils 
-						through 1.9.3, does not suppress the class property, which allows 
-						remote attackers to \"manipulate\" the ClassLoader and execute 
-						arbitrary code via the class parameter, as demonstrated by the passing 
-						of this parameter to the getClass method of the ActionForm object in 
-						Struts 1.",
+            "description": "Apache Commons BeanUtils, as distributed in lib/commons-beanutils-1.8.0.jar...",
             "recommendations": "",
             "packageId": "Maven-commons-beanutils:commons-beanutils-1.8.3",
             "recommendedVersion": "1.9.4",
