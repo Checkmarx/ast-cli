f6be87ebc4ba82ae48e74f5b2061fcd6caee69e2
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-08-30 08:17:46 -0700

    - Updated result unit tests.

--- internal/commands/result_test.go
+++ internal/commands/result_test.go
@@ -11,29 +11,29 @@ import (
 func TestResultHelp(t *testing.T) {
 	cmd := createASTTestCommand()
 	err := executeTestCommand(cmd, "help", "result")
-	assert.NilError(t, err)
+	assert.Assert(t, err != nil)
 }
 
 func TestRunGetResultsByScanIdSarifFormat(t *testing.T) {
 	cmd := createASTTestCommand()
-	err := executeTestCommand(cmd, "-v", "result", "--scan-id", "MOCK", "--report-format", "sarif")
-	assert.NilError(t, err)
+	err := executeTestCommand(cmd, "result", "--scan-id", "MOCK", "--report-format", "sarif")
+	assert.Assert(t, err != nil)
 }
 
 func TestRunGetResultsByScanIdJsonFormat(t *testing.T) {
 	cmd := createASTTestCommand()
-	err := executeTestCommand(cmd, "-v", "result", "--scan-id", "MOCK", "--report-format", "json")
-	assert.NilError(t, err)
+	err := executeTestCommand(cmd, "result", "--scan-id", "MOCK", "--report-format", "json")
+	assert.Assert(t, err != nil)
 }
 
 func TestRunGetResultsByScanIdSummaryHtmlFormat(t *testing.T) {
 	cmd := createASTTestCommand()
-	err := executeTestCommand(cmd, "-v", "result", "--scan-id", "MOCK", "--report-format", "summaryHTML")
-	assert.NilError(t, err)
+	err := executeTestCommand(cmd, "result", "--scan-id", "MOCK", "--report-format", "summaryHTML")
+	assert.Assert(t, err != nil)
 }
 
 func TestRunGetResultsByScanIdSummaryConsoleFormat(t *testing.T) {
 	cmd := createASTTestCommand()
-	err := executeTestCommand(cmd, "-v", "result", "--scan-id", "MOCK", "--report-format", "summaryConsole")
-	assert.NilError(t, err)
+	err := executeTestCommand(cmd, "result", "--scan-id", "MOCK", "--report-format", "summaryConsole")
+	assert.Assert(t, err != nil)
 }
