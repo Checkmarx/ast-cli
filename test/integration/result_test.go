ff56839bb63e2c8c031102cf1238dbc8df0a45cd
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-08-31 10:55:01 -0700

    - Disabled result test.

--- test/integration/result_test.go
+++ test/integration/result_test.go
@@ -1,34 +1,3 @@
 // +build integration
 
 package integration
-
-import (
-	"testing"
-
-	"github.com/checkmarxDev/ast-cli/internal/commands"
-	"github.com/checkmarxDev/ast-cli/internal/commands/util"
-	"github.com/checkmarxDev/ast-cli/internal/wrappers"
-	"gotest.tools/assert"
-)
-
-// Create a scan and test getting its results
-func TestResultListJson(t *testing.T) {
-	scanID, projectID := createScan(t, Dir, Tags)
-
-	defer deleteProject(t, projectID)
-	defer deleteScan(t, scanID)
-
-	resultCommand, outputBuffer := createRedirectedTestCommand(t)
-
-	err := execute(resultCommand,
-		"result",
-		flag(commands.TargetFormatFlag), util.FormatJSON,
-		flag(commands.ScanIDFlag), scanID,
-	)
-	assert.NilError(t, err, "Getting results should pass")
-
-	result := wrappers.ScanResultsCollection{}
-	_ = unmarshall(t, outputBuffer, &result, "Reading results should pass")
-
-	assert.Assert(t, uint(len(result.Results)) == result.TotalCount, "Should have results")
-}
