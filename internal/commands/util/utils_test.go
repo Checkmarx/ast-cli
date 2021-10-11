72e0f3bc555c63ad985d6c1989660829f1e8eb31
Hugo Machado <hugo.machado@checkmarx.com>
2021-10-11 15:32:58 +0100

    AST-5500 - CLI - Increase unit tests to 80%
    - Fixed lint errors

--- internal/commands/util/utils_test.go
+++ internal/commands/util/utils_test.go
@@ -4,15 +4,9 @@ import (
 	"testing"
 
 	"gotest.tools/assert"
-
-	"github.com/checkmarxDev/ast-cli/internal/params"
-	"github.com/checkmarxDev/ast-cli/internal/wrappers"
-	"github.com/spf13/viper"
 )
 
 func TestNewUtilsCommand(t *testing.T) {
-	logs := viper.GetString(params.LogsPathKey)
-	logsWrapper := wrappers.NewLogsWrapper(logs)
-	cmd := NewUtilsCommand(logsWrapper)
+	cmd := NewUtilsCommand()
 	assert.Assert(t, cmd != nil, "Utils command must exist")
 }
