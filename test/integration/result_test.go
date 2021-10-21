c55428043b137b007a993088645e9ee43b4cffca
hmmachadocx <91129244+hmmachadocx@users.noreply.github.com>
2021-10-21 13:34:23 +0100

    Feature/ast 5501 increase integration tests (#301)
    * AST-5500 - CLI - Increase unit tests to 80%

--- test/integration/result_test.go
+++ test/integration/result_test.go
@@ -21,40 +21,38 @@ const (
 
 // Create a scan and test getting its results
 func TestResultListJson(t *testing.T) {
+
+	assertRequiredParameter(t, "Please provide a scan ID", "result")
+
 	scanID, _ := getRootScan(t)
-	resultCommand, outputBuffer := createRedirectedTestCommand(t)
 
-	err := execute(
-		resultCommand,
-		"result",
-		flag(params.TargetFormatFlag), strings.Join([]string{util.FormatJSON, util.FormatSarif, util.FormatSummary, util.FormatSummaryConsole}, ","),
-		flag(params.TargetFlag), fileName,
-		flag(params.TargetPathFlag), resultsDirectory,
-	)
-	assertError(t, err, "Please provide a scan ID")
-
-	err = execute(
-		resultCommand,
+	outputBuffer := executeCmdNilAssertion(t, "Getting results should pass",
 		"result",
 		flag(params.TargetFormatFlag), strings.Join([]string{util.FormatJSON, util.FormatSarif, util.FormatSummary, util.FormatSummaryConsole}, ","),
 		flag(params.TargetFlag), fileName,
 		flag(params.ScanIDFlag), scanID,
 		flag(params.TargetPathFlag), resultsDirectory,
 	)
-	assert.NilError(t, err, "Getting results should pass")
-
-	extensions := []string{util.FormatJSON, util.FormatSarif, util.FormatHTML}
-	defer func() {
-		_ = os.RemoveAll(fmt.Sprintf(resultsDirectory))
-	}()
 
 	result := wrappers.ScanResultsCollection{}
 	_ = unmarshall(t, outputBuffer, &result, "Reading results should pass")
 
 	assert.Assert(t, uint(len(result.Results)) == result.TotalCount, "Should have results")
 
+	assertResultFilesCreated(t)
+}
+
+// assert all files were created
+func assertResultFilesCreated(t *testing.T) {
+	extensions := []string{util.FormatJSON, util.FormatSarif, util.FormatHTML}
+
 	for _, e := range extensions {
-		_, err = os.Stat(fmt.Sprintf("%s%s.%s", resultsDirectory, fileName, e))
+		_, err := os.Stat(fmt.Sprintf("%s%s.%s", resultsDirectory, fileName, e))
 		assert.NilError(t, err, "Report file should exist for extension "+e)
 	}
+
+	// delete directory in the end
+	defer func() {
+		_ = os.RemoveAll(fmt.Sprintf(resultsDirectory))
+	}()
 }
