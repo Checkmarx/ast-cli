3e345351c0c91b0a858ce44dc8e2ab73917af555
diogopcx <86957502+diogopcx@users.noreply.github.com>
2021-10-29 15:36:22 +0100

    AST-6537 Retry mechanism (#304)
    * AST-6537 Retry mechanism
    
    adds retry flag to set the number of retries should a request fail before exiting (default 0)
    adds retry-delay flag to set the seconds of sleep between each retry (default 3)
    
    * AST-6537 Retry mechanism
    
    linter issues
    
    * AST-6537 Retry mechanism
    
    fix comment location

--- test/integration/util_command.go
+++ test/integration/util_command.go
@@ -115,7 +115,7 @@ func executeCmdWithTimeOutNilAssertion(t *testing.T, infoMsg string, timeout tim
 
 func executeWithTimeout(cmd *cobra.Command, timeout time.Duration, args ...string) error {
 
-	args = append(args, flag(params.DebugFlag))
+	args = append(args, flag(params.DebugFlag), flag(params.RetryFlag), "3", flag(params.RetryDelayFlag), "5")
 	args = appendProxyArgs(args)
 	cmd.SetArgs(args)
 
