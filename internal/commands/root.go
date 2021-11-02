c75ed8264666277eb5864eabdcb9a766fd5376b0
Tiago Baptista <92083272+tiagobcx@users.noreply.github.com>
2021-11-02 18:00:03 +0000

    Adding autocomplete in goreleaser to homebrew (#307)
    * Adding autocomplete in goreleaser to homebrew

--- internal/commands/root.go
+++ internal/commands/root.go
@@ -70,7 +70,8 @@ func NewAstCLI(
 	// are passed to Cobra.
 	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
 		PrintConfiguration()
-		if len(args) > 0 && cmd.Name() != params.Help {
+		// Need to check the __complete command to allow correct behavior of the autocomplete
+		if len(args) > 0 && cmd.Name() != params.Help && cmd.Name() != "__complete" {
 			_ = cmd.Help()
 			os.Exit(0)
 		}
