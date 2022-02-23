ce7448b9a92a935ef79d4adb168b6cc83ed04f72
tiagobcx <tiago.baptista@checkmarx.com>
2022-02-23 09:41:44 +0000

    fix linter

--- internal/wrappers/codebashing-json.go
+++ internal/wrappers/codebashing-json.go
@@ -1,8 +1,8 @@
 package wrappers
 
 type CodeBashingCollection struct {
-	Path                 string               `json:"path,omitempty"`
-	CweId                string               `json:"cwe_id,omitempty"`
-	Lang                 string               `json:"lang,omitempty"`
-	CxQueryName          string               `json:"cxQueryName,omitempty"`
+	Path        string `json:"path,omitempty"`
+	CweId       string `json:"cwe_id,omitempty"`
+	Lang        string `json:"lang,omitempty"`
+	CxQueryName string `json:"cxQueryName,omitempty"`
 }
