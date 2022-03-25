991db2bc3effd5a1fabaa17e90df1785c9e3c993
tiagobcx <tiago.baptista@checkmarx.com>
2022-03-25 18:35:33 +0000

    fix linter

--- internal/commands/util/usercount/user-count.go
+++ internal/commands/util/usercount/user-count.go
@@ -37,7 +37,6 @@ func NewUserCountCommand(gitHubWrapper wrappers.GitHubWrapper,
 	azureWrapper wrappers.AzureWrapper,
 	bitBucketWrapper wrappers.BitBucketWrapper,
 	gitLabWrapper wrappers.GitLabWrapper) *cobra.Command {
-
 	userCountCmd := &cobra.Command{
 		Use:   UcCommand,
 		Short: UcShort,
