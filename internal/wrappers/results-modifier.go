c960e6ce10dd7a6bbc4ba4bf052c9e26103f6db3
tiagobcx <tiago.baptista@checkmarx.com>
2022-08-25 11:53:08 +0100

    adding check and debug message for negative results field

--- internal/wrappers/results-modifier.go
+++ internal/wrappers/results-modifier.go
@@ -3,8 +3,10 @@ package wrappers
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"strings"
 
+	"github.com/checkmarx/ast-cli/internal/logger"
 	"github.com/checkmarx/ast-cli/internal/params"
 )
 
@@ -48,7 +50,11 @@ func (s *ScanResult) UnmarshalJSON(data []byte) error {
 func (s *ScanResultNode) UnmarshalJSON(data []byte) error {
 	type Alias ScanResultNode
 	aux := &struct {
-		Column int `json:"column"`
+		Column     int    `json:"column"`
+		Line       int    `json:"line"`
+		Length     int    `json:"length"`
+		MethodLine int    `json:"methodLine"`
+		FileName   string `json:"fileName,omitempty"`
 		*Alias
 	}{
 		Alias: (*Alias)(s),
@@ -56,11 +62,34 @@ func (s *ScanResultNode) UnmarshalJSON(data []byte) error {
 	if err := json.Unmarshal(data, &aux); err != nil {
 		return err
 	}
-
 	s.Column = 0
+	s.Line = 0
+	s.Length = 0
+	s.MethodLine = 0
+	message := "Found negative %v with value %v in file %v"
 	if aux.Column >= 0 {
 		s.Column = uint(aux.Column)
+	} else {
+		message = fmt.Sprintf(message, "column", aux.Column, aux.FileName)
+		logger.PrintIfVerbose(message)
 	}
-
+	if aux.Line >= 0 {
+		s.Line = uint(aux.Line)
+	} else {
+		message = fmt.Sprintf(message, "line", aux.Line, aux.FileName)
+		logger.PrintIfVerbose(message)
+	}
+	if aux.Length >= 0 {
+		s.Length = uint(aux.Length)
+	} else {
+		message = fmt.Sprintf(message, "length", aux.Length, aux.FileName)
+		logger.PrintIfVerbose(message)
+	}
+	if aux.MethodLine >= 0 {
+		s.MethodLine = uint(aux.MethodLine)
+	} else {
+		message = fmt.Sprintf(message, "methodLine", aux.MethodLine, aux.FileName)
+		logger.PrintIfVerbose(message)
+	}
 	return nil
 }
