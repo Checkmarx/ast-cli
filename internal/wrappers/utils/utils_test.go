b5870eb596a7f880607987cd2248a1fae65c5168
igorlombacx <igor.lomba@checkmarx.com>
2022-10-12 14:38:11 +0100

    AST-17490 - scopelint fix

--- internal/wrappers/utils/utils_test.go
+++ internal/wrappers/utils/utils_test.go
@@ -41,6 +41,7 @@ func TestCleanURL(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := CleanURL(tt.args.uri)
 			log.Println(err)
