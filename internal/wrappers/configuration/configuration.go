2907966551abd3b5f025b8f35b4bc71c68e29aa2
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2022-11-02 17:48:05 +0000

    remove code

--- internal/wrappers/configuration/configuration.go
+++ internal/wrappers/configuration/configuration.go
@@ -129,11 +129,6 @@ func LoadConfiguration() {
 }
 
 func verifyConfigDir(fullPath string) {
-	_, err := os.Stat(fullPath)
-	if err != nil {
-		log.Fatal("Cannot file home directory.", fullPath)
-	}
-
 	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
 		fmt.Println("Creating directory")
 		err = os.Mkdir(fullPath, homeDirectoryPermissions)
