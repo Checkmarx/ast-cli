1b36ad6eea60b8d9414e0bc550c685efd5f44a90
igorlombacx <igor.lomba@checkmarx.com>
2022-11-29 13:01:22 +0000

    go parallel flag test

--- internal/commands/.scripts/integration_up.sh
+++ internal/commands/.scripts/integration_up.sh
@@ -10,7 +10,7 @@ wget https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-linux64.tar.g
 tar -xzvf ScaResolver-linux64.tar.gz -C /tmp
 rm -rf ScaResolver-linux64.tar.gz
 
-go test -p \
+go test \
   -tags integration \
   -v \
   -timeout 60m \
