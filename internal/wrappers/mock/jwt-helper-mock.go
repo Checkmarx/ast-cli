5f0d7c07ee2b13132f97aea859c23df5ba4f29f8
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2023-01-17 12:24:44 +0000

    fix unit test

--- internal/wrappers/mock/jwt-helper-mock.go
+++ internal/wrappers/mock/jwt-helper-mock.go
@@ -9,7 +9,7 @@ type JWTMockWrapper struct{}
 // GetAllowedEngines mock for tests
 func (*JWTMockWrapper) GetAllowedEngines() (allowedEngines map[string]bool, err error) {
 	allowedEngines = make(map[string]bool)
-	engines := []string{"sast", "kics", "sca", "api-security"}
+	engines := []string{"sast", "iac-security", "sca", "api-security"}
 	for _, value := range engines {
 		allowedEngines[strings.ToLower(value)] = true
 	}
