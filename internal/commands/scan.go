95ceabebf83489e241bb050c199f413b437597a9
Pedro Lopes <83576881+pedrompflopes@users.noreply.github.com>
2023-01-27 15:12:33 +0000

    Clean scan types (#496)
    * remove spaces from user scan types
    
    * don't ignore error parsing the token

--- internal/commands/scan.go
+++ internal/commands/scan.go
@@ -831,6 +831,7 @@ func validateScanTypes(cmd *cobra.Command, jwtWrapper wrappers.JWTWrapper) error
 
 	userScanTypes, _ := cmd.Flags().GetString(commonParams.ScanTypes)
 	if len(userScanTypes) > 0 {
+		userScanTypes = strings.ReplaceAll(strings.ToLower(userScanTypes), " ", "")
 		userScanTypes = strings.Replace(strings.ToLower(userScanTypes), commonParams.KicsType, commonParams.IacType, 1)
 
 		scanTypes = strings.Split(userScanTypes, ",")
