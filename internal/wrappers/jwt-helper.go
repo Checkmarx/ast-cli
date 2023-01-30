528292f383a8cd052fe95fac04b070c16afb430c
igorlombacx <igor.lomba@checkmarx.com>
2023-01-30 18:01:41 +0000

    BUG-JWT-Claims fix

--- internal/wrappers/jwt-helper.go
+++ internal/wrappers/jwt-helper.go
@@ -1,6 +1,7 @@
 package wrappers
 
 import (
+	"encoding/json"
 	"strings"
 
 	commonParams "github.com/checkmarx/ast-cli/internal/params"
@@ -11,63 +12,26 @@ import (
 
 // JWTStruct model used to get all jwt fields
 type JWTStruct struct {
-	Exp            int      `json:"exp"`
-	Iat            int      `json:"iat"`
-	Jti            string   `json:"jti"`
-	Iss            string   `json:"iss"`
-	Aud            []string `json:"aud"`
-	Sub            string   `json:"sub"`
-	Typ            string   `json:"typ"`
-	Azp            string   `json:"azp"`
-	SessionState   string   `json:"session_state"`
-	AllowedOrigins []string `json:"allowed-origins"`
-	ResourceAccess struct {
-		RealmManagement struct {
-			Roles []string `json:"roles"`
-		} `json:"realm-management"`
-		Account struct {
-			Roles []string `json:"roles"`
-		} `json:"account"`
-	} `json:"resource_access"`
-	Scope             string        `json:"scope"`
-	Sid               string        `json:"sid"`
-	TenantID          string        `json:"tenant_id"`
-	TenantName        string        `json:"tenant_name"`
-	EmailVerified     bool          `json:"email_verified"`
-	Roles             []string      `json:"roles"`
-	EulaAccepted      bool          `json:"eula-accepted"`
-	Groups            []interface{} `json:"groups"`
-	GroupsNames       []interface{} `json:"groupsNames"`
-	CbURL             string        `json:"cb-url"`
-	PreferredUsername string        `json:"preferred_username"`
-	GivenName         string        `json:"given_name"`
-	AstBaseURL        string        `json:"ast-base-url"`
-	SfID              string        `json:"sf-id"`
-	RolesAst          []string      `json:"roles_ast"`
-	Name              string        `json:"name"`
-	TenantType        string        `json:"tenant-type"`
-	AstLicense        struct {
-		ID          int    `json:"ID"`
-		TenantID    string `json:"TenantID"`
-		IsActive    bool   `json:"IsActive"`
-		PackageID   int    `json:"PackageID"`
-		LicenseData struct {
-			Features                        []string `json:"features"`
-			Services                        []string `json:"services"`
-			UsersCount                      int      `json:"usersCount"`
-			ServiceType                     string   `json:"serviceType"`
-			ActivationDate                  int64    `json:"activationDate"`
-			AllowedEngines                  []string `json:"allowedEngines"`
-			CodeBashingURL                  string   `json:"codeBashingUrl"`
-			ExpirationDate                  int64    `json:"expirationDate"`
-			UnlimitedProjects               bool     `json:"unlimitedProjects"`
-			CodeBashingEnabled              bool     `json:"codeBashingEnabled"`
-			MaxConcurrentScans              int      `json:"maxConcurrentScans"`
-			CodeBashingUsersCount           int      `json:"codeBashingUsersCount"`
-			CustomMaxConcurrentScansEnabled bool     `json:"customMaxConcurrentScansEnabled"`
-		} `json:"LicenseData"`
-		PackageName string `json:"PackageName"`
-	} `json:"ast-license"`
+	ID          int    `json:"ID"`
+	TenantID    string `json:"TenantID"`
+	IsActive    bool   `json:"IsActive"`
+	PackageID   int    `json:"PackageID"`
+	LicenseData struct {
+		Features                        []string `json:"features"`
+		Services                        []string `json:"services"`
+		UsersCount                      int      `json:"usersCount"`
+		ServiceType                     string   `json:"serviceType"`
+		ActivationDate                  int64    `json:"activationDate"`
+		AllowedEngines                  []string `json:"allowedEngines"`
+		CodeBashingURL                  string   `json:"codeBashingUrl"`
+		ExpirationDate                  int64    `json:"expirationDate"`
+		UnlimitedProjects               bool     `json:"unlimitedProjects"`
+		CodeBashingEnabled              bool     `json:"codeBashingEnabled"`
+		MaxConcurrentScans              int      `json:"maxConcurrentScans"`
+		CodeBashingUsersCount           int      `json:"codeBashingUsersCount"`
+		CustomMaxConcurrentScansEnabled bool     `json:"customMaxConcurrentScansEnabled"`
+	} `json:"LicenseData"`
+	PackageName string `json:"PackageName"`
 	jwt.StandardClaims
 }
 
@@ -91,8 +55,7 @@ func (*JWTStruct) GetAllowedEngines() (allowedEngines map[string]bool, err error
 	if err != nil {
 		return nil, err
 	}
-	allowedEngines = prepareEngines(jwtStruct.AstLicense.LicenseData.AllowedEngines)
-
+	allowedEngines = prepareEngines(jwtStruct.LicenseData.AllowedEngines)
 	return allowedEngines, nil
 }
 
@@ -112,10 +75,18 @@ func prepareEngines(engines []string) map[string]bool {
 
 // extractFromTokenToJwtStruct used in scan validation
 func extractFromTokenToJwtStruct(accessToken string) (*JWTStruct, error) {
-	claims := JWTStruct{}
-	_, _, err := new(jwt.Parser).ParseUnverified(accessToken, &claims)
+	value := &JWTStruct{}
+	token, _, err := new(jwt.Parser).ParseUnverified(accessToken, jwt.MapClaims{})
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && claims["ast-license"] != nil {
+		astLicenseClaim := claims["ast-license"]
+		valueBytes, _ := json.Marshal(astLicenseClaim)
+		err = json.Unmarshal(valueBytes, &value)
+		if err != nil {
+			return nil, errors.Errorf(APIKeyDecodeErrorFormat, err)
+		}
+	}
 	if err != nil {
 		return nil, errors.Errorf(APIKeyDecodeErrorFormat, err)
 	}
-	return &claims, nil
+	return value, nil
 }
