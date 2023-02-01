7b6d12ea5dd2214eb3027073f068b194ae4244f4
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2023-02-01 16:24:26 +0000

    disable jwt enforcement

--- internal/wrappers/jwt-helper.go
+++ internal/wrappers/jwt-helper.go
@@ -14,7 +14,7 @@ type JWTStruct struct {
 	jwt.Claims
 }
 
-var enabledEngines = []string{"sast", "sca", "api-security", "iac-security"}
+// var enabledEngines = []string{"sast", "sca", "api-security", "iac-security"}
 var defaultEngines = map[string]bool{
 	"sast":         true,
 	"sca":          true,
@@ -34,44 +34,46 @@ func (*JWTStruct) GetAllowedEngines() (allowedEngines map[string]bool, err error
 	return defaultEngines, nil
 }
 
-//// GetAllowedEngines will return a map with user allowed engines
-//func (*JWTStruct) GetAllowedEngines() (allowedEngines map[string]bool, err error) {
-//	accessToken, err := GetAccessToken()
-//	if err != nil {
-//		return nil, err
-//	}
-//	jwtStruct, err := extractFromTokenToJwtStruct(accessToken)
-//	if err != nil {
-//		return nil, err
-//	}
-//	_ = prepareEngines(jwtStruct.AstLicense.LicenseData.AllowedEngines)
-//	return allowedEngines, nil
-//}
-//
-//func prepareEngines(engines []string) map[string]bool {
-//	m := make(map[string]bool)
-//	for _, value := range engines {
-//		engine := strings.Replace(strings.ToLower(value), strings.ToLower(commonParams.APISecurityLabel), commonParams.APISecurityType, 1)
-//		engine = strings.Replace(strings.ToLower(engine), commonParams.KicsType, commonParams.IacType, 1)
-//
-//		// Current limitation, CxOne is including non-engines in the JWT
-//		if utils.Contains(enabledEngines, strings.ToLower(engine)) {
-//			m[strings.ToLower(engine)] = true
-//		}
-//	}
-//	return m
-//}
-//
-//func extractFromTokenToJwtStruct(accessToken string) (*JWTStruct, error) {
-//	token, _, err := new(jwt.Parser).ParseUnverified(accessToken, &JWTStruct{})
-//	if err != nil {
-//		return nil, errors.Errorf(APIKeyDecodeErrorFormat, err)
-//	}
-//
-//	claims, ok := token.Claims.(*JWTStruct)
-//	if !ok {
-//		return nil, errors.Errorf(APIKeyDecodeErrorFormat, err)
-//	}
-//
-//	return claims, nil
-//}
+/**
+// GetAllowedEngines will return a map with user allowed engines
+func (*JWTStruct) GetAllowedEngines() (allowedEngines map[string]bool, err error) {
+	accessToken, err := GetAccessToken()
+	if err != nil {
+		return nil, err
+	}
+	jwtStruct, err := extractFromTokenToJwtStruct(accessToken)
+	if err != nil {
+		return nil, err
+	}
+	_ = prepareEngines(jwtStruct.AstLicense.LicenseData.AllowedEngines)
+	return allowedEngines, nil
+}
+
+func prepareEngines(engines []string) map[string]bool {
+	m := make(map[string]bool)
+	for _, value := range engines {
+		engine := strings.Replace(strings.ToLower(value), strings.ToLower(commonParams.APISecurityLabel), commonParams.APISecurityType, 1)
+		engine = strings.Replace(strings.ToLower(engine), commonParams.KicsType, commonParams.IacType, 1)
+
+		// Current limitation, CxOne is including non-engines in the JWT
+		if utils.Contains(enabledEngines, strings.ToLower(engine)) {
+			m[strings.ToLower(engine)] = true
+		}
+	}
+	return m
+}
+
+func extractFromTokenToJwtStruct(accessToken string) (*JWTStruct, error) {
+	token, _, err := new(jwt.Parser).ParseUnverified(accessToken, &JWTStruct{})
+	if err != nil {
+		return nil, errors.Errorf(APIKeyDecodeErrorFormat, err)
+	}
+
+	claims, ok := token.Claims.(*JWTStruct)
+	if !ok {
+		return nil, errors.Errorf(APIKeyDecodeErrorFormat, err)
+	}
+
+	return claims, nil
+}
+**/
