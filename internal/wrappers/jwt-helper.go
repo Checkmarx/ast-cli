b5e5d85c7e2672faa284cb4e17408d36f9a9009e
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2023-02-06 12:46:35 +0000

    jwt enforcement

--- internal/wrappers/jwt-helper.go
+++ internal/wrappers/jwt-helper.go
@@ -1,7 +1,12 @@
 package wrappers
 
 import (
+	"strings"
+
+	commonParams "github.com/checkmarx/ast-cli/internal/params"
+	"github.com/checkmarx/ast-cli/internal/wrappers/utils"
 	"github.com/golang-jwt/jwt"
+	"github.com/pkg/errors"
 )
 
 // JWTStruct model used to get all jwt fields
@@ -14,12 +19,7 @@ type JWTStruct struct {
 	jwt.Claims
 }
 
-var defaultEngines = map[string]bool{
-	"sast":         true,
-	"sca":          true,
-	"api-security": true,
-	"iac-security": true,
-}
+var enabledEngines = []string{"sast", "sca", "api-security", "iac-security"}
 
 type JWTWrapper interface {
 	GetAllowedEngines() (allowedEngines map[string]bool, err error)
@@ -29,6 +29,44 @@ func NewJwtWrapper() JWTWrapper {
 	return &JWTStruct{}
 }
 
+// GetAllowedEngines will return a map with user allowed engines
 func (*JWTStruct) GetAllowedEngines() (allowedEngines map[string]bool, err error) {
-	return defaultEngines, nil
+	accessToken, err := GetAccessToken()
+	if err != nil {
+		return nil, err
+	}
+	jwtStruct, err := extractFromTokenToJwtStruct(accessToken)
+	if err != nil {
+		return nil, err
+	}
+	allowedEngines = prepareEngines(jwtStruct.AstLicense.LicenseData.AllowedEngines)
+	return allowedEngines, nil
+}
+
+func prepareEngines(engines []string) map[string]bool {
+	m := make(map[string]bool)
+	for _, value := range engines {
+		engine := strings.Replace(strings.ToLower(value), strings.ToLower(commonParams.APISecurityLabel), commonParams.APISecurityType, 1)
+		engine = strings.Replace(strings.ToLower(engine), commonParams.KicsType, commonParams.IacType, 1)
+
+		// Current limitation, CxOne is including non-engines in the JWT
+		if utils.Contains(enabledEngines, strings.ToLower(engine)) {
+			m[strings.ToLower(engine)] = true
+		}
+	}
+	return m
+}
+
+func extractFromTokenToJwtStruct(accessToken string) (*JWTStruct, error) {
+	token, _, err := new(jwt.Parser).ParseUnverified(accessToken, &JWTStruct{})
+	if err != nil {
+		return nil, errors.Errorf(APIKeyDecodeErrorFormat, err)
+	}
+
+	claims, ok := token.Claims.(*JWTStruct)
+	if !ok {
+		return nil, errors.Errorf(APIKeyDecodeErrorFormat, err)
+	}
+
+	return claims, nil
 }
