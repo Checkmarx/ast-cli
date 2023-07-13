bee4e2765acee8398ea8f0e368e65ce62cc93a06
Pedro Lopes <83576881+pedrompflopes@users.noreply.github.com>
2023-07-13 15:34:30 +0100

    fix sbom (#555)
    * fix sbom

--- internal/wrappers/results-sbom.go
+++ internal/wrappers/results-sbom.go
@@ -2,7 +2,7 @@ package wrappers
 
 type ResultsSbomWrapper interface {
 	GenerateSbomReport(payload *SbomReportsPayload) (*SbomReportsResponse, error)
-	GenerateSbomReportWithProxy(payload *SbomReportsPayload, targetFile string) error
+	GenerateSbomReportWithProxy(payload *SbomReportsPayload, targetFile string) (bool, error)
 	GetSbomReportStatus(reportID string) (*SbomPollingResponse, error)
 	DownloadSbomReport(reportID, targetFile string) error
 }
