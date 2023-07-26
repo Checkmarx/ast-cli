899382ff5f171cd03f2c07584080fa4504886a4d
Tiago Baptista <92083272+tiagobcx@users.noreply.github.com>
2023-07-26 16:12:08 +0100

    Policy evaluation (#553)

---
+++ internal/wrappers/mock/policy-mock.go
@@ -0,0 +1,29 @@
+package mock
+
+import "github.com/checkmarx/ast-cli/internal/wrappers"
+
+type PolicyMockWrapper struct {
+}
+
+func (r *PolicyMockWrapper) EvaluatePolicy(params map[string]string) (
+	*wrappers.PolicyResponseModel,
+	*wrappers.WebError,
+	error,
+) {
+	policyResponseModel := wrappers.PolicyResponseModel{}
+	policyResponseModel.BreakBuild = false
+	policyResponseModel.Status = "COMPLETED"
+
+	policy := wrappers.Policy{}
+	policy.Name = "MOCK_NAME"
+	policy.RulesViolated = make([]string, 0)
+	policy.BreakBuild = false
+	policy.Description = "MOCK_DESC"
+	policy.Tags = make([]string, 0)
+
+	var policies []wrappers.Policy
+	policies = append(policies, policy)
+	policyResponseModel.Polices = policies
+
+	return &policyResponseModel, nil, nil
+}
