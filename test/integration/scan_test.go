eda34868905665ade86f2ff2bc3ddec25078ae73
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2023-11-24 10:26:30 +0000

    improve testing

--- test/integration/scan_test.go
+++ test/integration/scan_test.go
@@ -177,7 +177,7 @@ func TestCancelScan(t *testing.T) {
 	// canceling too quickly after creating fails the scan...
 	time.Sleep(30 * time.Second)
 
-	assert.Assert(t, pollScanUntilStatus(t, scanID, wrappers.ScanCanceled, 200, 5), "Scan should be running before cancel")
+	assert.Assert(t, pollScanUntilStatus(t, scanID, wrappers.ScanRunning, 200, 5), "Scan should be running before cancel")
 
 	executeCmdNilAssertion(t, "Cancel should pass", "scan", "cancel", flag(params.ScanIDFlag), scanID)
 
