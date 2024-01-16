72c168744dc113080a25eb0ef4eb09bcb924a4e7
Ittai Gilat <ittai.gilat@checkmarx.com>
2024-01-16 11:54:46 +0200

    Add caht-sast integration test

--- test/integration/chat_test.go
+++ test/integration/chat_test.go
@@ -28,3 +28,18 @@ func TestChatInvalidAPIKey(t *testing.T) {
 	unmarshall(t, respBuffer, &outputModel, "Reading results should pass")
 	assert.Assert(t, strings.Contains(outputModel.Response[0], "Incorrect API key provided"), "Expecting incorrect api key error")
 }
+
+func TestChatInvalidAPIKey(t *testing.T) {
+	args := []string{
+		"chat", "sast",
+		"--chat-apikey", "invalidApiKey",
+		"--scan-results-file", "./data/cx_result.json",
+		"--source-dir", "./data",
+		"--sast-result-id", "13588362",
+	}
+	err, respBuffer := executeCommand(t, args...)
+	assert.NilError(t, err)
+	outputModel := commands.OutputModel{}
+	unmarshall(t, respBuffer, &outputModel, "Reading results should pass")
+	assert.Assert(t, strings.Contains(outputModel.Response[0], "Incorrect API key provided"), "Expecting incorrect api key error")
+}
