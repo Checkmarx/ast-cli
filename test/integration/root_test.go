302cb6906f9adc53f937bba7ec85e456f0822c48
AlvoBen <144705560+AlvoBen@users.noreply.github.com>
2024-02-19 12:38:12 +0200

    solve getRoot function test bugs

--- test/integration/root_test.go
+++ test/integration/root_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/spf13/viper"
@@ -58,7 +59,7 @@ func getRootScan(t *testing.T, scanTypes ...string) (string, string) {
 		rootScanId, rootScanProjectId = createScan(testInstance, Zip, Tags)
 		return rootScanId, rootScanProjectId
 	} else {
-		rootEnginesScanId, rootEnginesScanProjectId = createScanWithEngines(testInstance, Zip, Tags, slices.join(scanTypes, ","))
+		rootEnginesScanId, rootEnginesScanProjectId = createScanWithEngines(testInstance, Zip, Tags, strings.Join(scanTypes, ","))
 		return rootEnginesScanId, rootEnginesScanProjectId
 
 	}
