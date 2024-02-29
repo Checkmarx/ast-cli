3d4e2fcd3d42a66a139645a890565f5617e4bbf4
tamarleviCm <tamar.levi@checkmarx.com>
2024-02-29 13:50:29 +0200

    enable Ai Guided Remediation only if the tenant has permission

--- internal/commands/chat.go
+++ internal/commands/chat.go
@@ -1,21 +1,43 @@
 package commands
 
 import (
+	"strconv"
+
 	"github.com/checkmarx/ast-cli/internal/wrappers"
 	"github.com/spf13/cobra"
 )
 
 const ConversationIDErrorFormat = "Invalid conversation ID %s"
+const AiGuidedRemediationEnabled = "scan.config.plugins.aiGuidedRemediation"
 
-func NewChatCommand(chatWrapper wrappers.ChatWrapper) *cobra.Command {
+func NewChatCommand(chatWrapper wrappers.ChatWrapper, tenantWrapper wrappers.TenantConfigurationWrapper) *cobra.Command {
 	chatCmd := &cobra.Command{
 		Use:   "chat",
 		Short: "Chat with OpenAI models",
 		Long:  "Chat with OpenAI models regarding KICS or SAST results",
 	}
-	chatKicsCmd := ChatKicsSubCommand(chatWrapper)
-	chatSastCmd := ChatSastSubCommand(chatWrapper)
-
-	chatCmd.AddCommand(chatKicsCmd, chatSastCmd)
+	chatCmd.AddCommand(ChatKicsSubCommand(chatWrapper))
+	if aiGuidedRemediationEnabled(tenantWrapper) {
+		chatCmd.AddCommand(ChatSastSubCommand(chatWrapper))
+	}
 	return chatCmd
+}
+
+func aiGuidedRemediationEnabled(tenantWrapper wrappers.TenantConfigurationWrapper) bool {
+	tenantConfigurationResponse, errorModel, err := tenantWrapper.GetTenantConfiguration()
+	if err != nil {
+		return false
+	}
+	if errorModel != nil {
+		return false
+	}
+	if tenantConfigurationResponse != nil {
+		for _, resp := range *tenantConfigurationResponse {
+			if resp.Key == AiGuidedRemediationEnabled {
+				isEnabled, _ := strconv.ParseBool(resp.Value)
+				return isEnabled
+			}
+		}
+	}
+	return false
 }
