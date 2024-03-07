d9b101912691e736ba9d8f3ba0704ae4b3b7d1af
checkmarx-kobi-hagmi <144018503+checkmarx-kobi-hagmi@users.noreply.github.com>
2024-03-07 15:19:43 +0200

    Update Get Application by Name with Exact Match and Update Permissions Log Message (AST-36823) (#669)
    * the application name passed to application-name flag must match application name exactly
    
    * Changed ApplicationDoesntExist and ApplicationNoPermission to ApplicationDoesntExistOrNoPermission
    
    * Fixed lint error
    
    ---------
    
    Co-authored-by: Or Shamir Checkmarx <93518641+OrShamirCM@users.noreply.github.com>

--- internal/errors/application-errors.go
+++ internal/errors/application-errors.go
@@ -1,8 +1,7 @@
 package applicationerrors
 
 const (
-	ApplicationDoesntExist  = "Provided application does not exist"
-	ApplicationNoPermission = "User have no permission to the application"
+	ApplicationDoesntExistOrNoPermission = "Provided application does not exist or user has no permission to the application"
 )
 
 const (
--- test/integration/project_test.go
+++ test/integration/project_test.go
@@ -101,7 +101,7 @@ func TestProjectCreate_ApplicationDoesntExist_FailAndReturnErrorMessage(t *testi
 		flag(params.ApplicationName), "application-that-doesnt-exist",
 	)
 
-	assertError(t, err, applicationErrors.ApplicationDoesntExist)
+	assertError(t, err, applicationErrors.ApplicationDoesntExistOrNoPermission)
 }
 
 func TestProjectCreate_ApplicationExists_CreateProjectSuccessfully(t *testing.T) {
