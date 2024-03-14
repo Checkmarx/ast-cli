e71b1457093599bffd5983990cb289520bff29ee
AlvoBen <alvo@post.bgu.ac.il>
2024-03-14 10:50:33 +0200

    improve utils_test.go coverage

--- internal/commands/util/utils_test.go
+++ internal/commands/util/utils_test.go
@@ -13,3 +13,19 @@ func TestNewUtilsCommand(t *testing.T) {
 		nil, nil, nil, nil, nil, nil, nil)
 	assert.Assert(t, cmd != nil, "Utils command must exist")
 }
+
+func TestCompressFile(t *testing.T) {
+	_, err := CompressFile("package.json", "package.json")
+	assert.NilError(t, err, "CompressFile must run well")
+}
+
+// test ReadFileAsString
+func TestReadFileAsString_Success(t *testing.T) {
+	_, err := ReadFileAsString("../data/package.json")
+	assert.NilError(t, err, "ReadFileAsString must run well")
+}
+
+func TestReadFileAsString_NoFile_Fail(t *testing.T) {
+	_, err := ReadFileAsString("no-file-exists-with-this-name.json")
+	assert.Error(t, err, "open no-file-exists-with-this-name.json: no such file or directory")
+}
