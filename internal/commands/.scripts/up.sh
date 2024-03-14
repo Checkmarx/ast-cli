fa9102826a0c1c28ef29c024391db8347d49ffb4
AlvoBen <alvo@post.bgu.ac.il>
2024-03-14 09:55:26 +0200

    update up.sh script to run the old version of go coverage

--- internal/commands/.scripts/up.sh
+++ internal/commands/.scripts/up.sh
@@ -4,4 +4,4 @@ wget https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-linux64.tar.g
 tar -xzvf ScaResolver-linux64.tar.gz -C /tmp
 rm -rf ScaResolver-linux64.tar.gz
 
-GOEXPERIMENT=nocoverageredesign go test -cover ./...
+GOEXPERIMENT=nocoverageredesign go test ./... -coverprofile cover.out
