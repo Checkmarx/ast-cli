9a9bc3bd391e5a65353a01215853f5ec747efa78
checkmarx-kobi-hagmi <kobi.hagmi@checkmarx.com>
2024-03-20 14:57:47 +0200

    Fixed vulcheck

--- internal/wrappers/mock/byor-mock.go
+++ internal/wrappers/mock/byor-mock.go
@@ -2,19 +2,21 @@ package mock
 
 import (
 	"fmt"
+
+	errors2 "github.com/checkmarx/ast-cli/internal/constants/errors"
 )
 
 type ByorMockWrapper struct{}
 
 func (b *ByorMockWrapper) Import(projectID, uploadURL string) (string, error) {
 	if projectID == FakeUnauthorized401 {
-		return "", fmt.Errorf(errors.StatusUnauthorized)
+		return "", fmt.Errorf(errors2.StatusUnauthorized)
 	}
 	if projectID == FakeForbidden403 {
-		return "", fmt.Errorf(errors.StatusForbidden)
+		return "", fmt.Errorf(errors2.StatusForbidden)
 	}
 	if projectID == FakeInternalServerError500 {
-		return "", fmt.Errorf(errors.StatusInternalServerError)
+		return "", fmt.Errorf(errors2.StatusInternalServerError)
 	}
 	return "", nil
 }
