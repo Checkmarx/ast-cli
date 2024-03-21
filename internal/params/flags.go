29602c0f4c78057cc568b7edacb8cfba88861416
tamarleviCm <tamar.levi@checkmarx.com>
2024-03-21 11:19:51 +0200

    containers type

--- internal/params/flags.go
+++ internal/params/flags.go
@@ -224,7 +224,6 @@ const (
 	SastType             = "sast"
 	KicsType             = "kics"
 	APISecurityType      = "api-security"
-	ContainersType       = "containers"
 	APIDocumentationFlag = "apisec-swagger-filter"
 	IacType              = "iac-security"
 	IacLabel             = "IaC Security"
