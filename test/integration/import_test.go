71ed5097ca870fc54c0759f2f0c18224fa3b27e7
checkmarx-kobi-hagmi <kobi.hagmi@checkmarx.com>
2024-03-28 16:30:42 +0200

    Tests to support BYOR FF

--- test/integration/import_test.go
+++ test/integration/import_test.go
@@ -6,36 +6,52 @@ import (
 	"testing"
 
 	errorConstants "github.com/checkmarx/ast-cli/internal/constants/errors"
+	featureFlagsConstants "github.com/checkmarx/ast-cli/internal/constants/feature-flags"
 	"github.com/checkmarx/ast-cli/internal/params"
+	"github.com/checkmarx/ast-cli/internal/wrappers"
 	"gotest.tools/assert"
 )
 
 func TestImport_ImportSarifFileWithCorrectFlags_CreateImportSuccessfully(t *testing.T) {
+
 	projectId, projectName := createProject(t, nil, nil)
+	defer deleteProject(t, projectId)
+
+	if !wrappers.FeatureFlags[featureFlagsConstants.ByorEnabled] {
+		t.Skip("BYOR flag is currently disabled")
+	}
 	args := []string{
 		"import",
 		flag(params.ProjectName), projectName,
 		flag(params.ImportFilePath), ".\\data\\sarif.sarif",
 	}
 	err, _ := executeCommand(t, args...)
-	defer deleteProject(t, projectId)
+
 	assert.NilError(t, err, "Import command failed with existing project")
 }
 
 func TestImport_ImportSarifFileWithCorrectFlagsZipFileExtention_CreateImportSuccessfully(t *testing.T) {
 	projectId, projectName := createProject(t, nil, nil)
+	defer deleteProject(t, projectId)
+
+	if !wrappers.FeatureFlags[featureFlagsConstants.ByorEnabled] {
+		t.Skip("BYOR flag is currently disabled")
+	}
+
 	args := []string{
 		"import",
 		flag(params.ProjectName), projectName,
 		flag(params.ImportFilePath), ".\\data\\sarif.zip",
 	}
 	err, _ := executeCommand(t, args...)
-	defer deleteProject(t, projectId)
+
 	assert.NilError(t, err, "Import command failed with existing project")
 }
 
 func TestImport_ImportSarifFileProjectDoesntExist_CreateImportWithProvidedNewNameSuccessfully(t *testing.T) {
+	t.Skip("BYOR flag is currently disabled")
 	projectName := projectNameRandom
+	defer deleteProjectByName(t, projectName)
 
 	args := []string{
 		"import",
@@ -43,12 +59,14 @@ func TestImport_ImportSarifFileProjectDoesntExist_CreateImportWithProvidedNewNam
 		flag(params.ImportFilePath), ".\\data\\sarif.sarif",
 	}
 	err, _ := executeCommand(t, args...)
-	defer deleteProjectByName(t, projectName)
+
 	assert.NilError(t, err, "Import command failed with non-existing project")
 }
 
 func TestImport_ImportSarifFileMissingVersion_ImportFailWithCorrectMessage(t *testing.T) {
+	t.Skip("BYOR flag is currently disabled")
 	projectName := projectNameRandom
+	defer deleteProjectByName(t, projectName)
 
 	args := []string{
 		"import",
@@ -56,11 +74,12 @@ func TestImport_ImportSarifFileMissingVersion_ImportFailWithCorrectMessage(t *te
 		flag(params.ImportFilePath), ".\\data\\sarif-missing-version.sarif",
 	}
 	err, _ := executeCommand(t, args...)
-	defer deleteProjectByName(t, projectName)
+
 	assertError(t, err, errorConstants.ImportSarifFileErrorMessage)
 }
 
 func TestImport_ImportMalformedSarifFile_ImportFailWithCorrectMessage(t *testing.T) {
+	t.Skip("BYOR flag is currently disabled")
 	projectName := projectNameRandom
 
 	args := []string{
@@ -69,6 +88,6 @@ func TestImport_ImportMalformedSarifFile_ImportFailWithCorrectMessage(t *testing
 		flag(params.ImportFilePath), ".\\data\\malformed-sarif.sarif",
 	}
 	err, _ := executeCommand(t, args...)
-	deleteProjectByName(t, projectName)
+	defer deleteProjectByName(t, projectName)
 	assertError(t, err, errorConstants.ImportSarifFileErrorMessage)
 }
