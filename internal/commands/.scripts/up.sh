bb79bfbd29b7e25ef89a5732b495ff4db644dc1f
tamarleviCm <tamar.levi@checkmarx.com>
2024-03-28 12:24:28 +0200

    ignore wrappers and mock packages in test coverage

--- internal/commands/.scripts/up.sh
+++ internal/commands/.scripts/up.sh
@@ -4,4 +4,4 @@ wget https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-linux64.tar.g
 tar -xzvf ScaResolver-linux64.tar.gz -C /tmp
 rm -rf ScaResolver-linux64.tar.gz
 
-go test ./... -coverprofile cover.out
+go test $(go list ./... | grep -v "mock" | grep -v "wrappers") -coverprofile cover.out
