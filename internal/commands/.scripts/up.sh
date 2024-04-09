f7143315918ac69ad5ebb2294a615ba8f64059ae
tamarleviCm <110327792+tamarleviCm@users.noreply.github.com>
2024-04-09 19:21:27 +0200

    AST-37687 FedRAMP - upgrade go version and alpine image (#689)
    * go version and alpine image
    
    * do not ignore tests
    
    * adding integration tag to tests
    
    * do not ignore tests
    
    * ignore wrappers and mock packages in test coverage
    
    * ignore wrappers and mock packages in test coverage
    
    * ignore logger package in test coverage
    
    * run release on the fips image
    
    * try
    
    * docker file
    
    * comment
    
    * integration build
    
    * run the release with public golden image
    
    * delete comment
    
    * go version from go.mod
    
    * policy tests
    
    * upgrade crypto
    
    * lint
    
    * printer_test
    
    * testify
    
    * fix linter
    
    * linter
    
    * pin test
    
    * pin test
    
    * pin test
    
    * lint
    
    * lint
    
    * test
    
    * test
    
    ---------
    
    Co-authored-by: Or Shamir Checkmarx <93518641+OrShamirCM@users.noreply.github.com>

--- internal/commands/.scripts/up.sh
+++ internal/commands/.scripts/up.sh
@@ -3,5 +3,5 @@
 wget https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-linux64.tar.gz
 tar -xzvf ScaResolver-linux64.tar.gz -C /tmp
 rm -rf ScaResolver-linux64.tar.gz
-
-go test ./... -coverprofile cover.out
+# ignore mock and wrappers packages, as they checked by integration tests
+go test $(go list ./... | grep -v "mock" | grep -v "wrappers" | grep -v "bitbucketserver" | grep -v "logger") -coverprofile cover.out
