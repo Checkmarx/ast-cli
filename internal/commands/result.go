528867ed3176ec317828eb4e6e8d9805285818fc
checkmarx-kobi-hagmi <kobi.hagmi@checkmarx.com>
2024-04-10 18:01:14 +0300

    not printing General

--- internal/commands/result.go
+++ internal/commands/result.go
@@ -323,7 +323,7 @@ func createRequestedScannersResponse(scanTypes map[string]string, scanResponseMo
 func createAllFailedScannersResponse(scanResponseModel *wrappers.ScanResponseModel) []ScannerResponse {
 	var results []ScannerResponse
 	for i := range scanResponseModel.StatusDetails {
-		if scanResponseModel.StatusDetails[i].Status == wrappers.ScanFailed {
+		if scanResponseModel.StatusDetails[i].Status == wrappers.ScanFailed && scanResponseModel.StatusDetails[i].Name != General {
 			results = append(results, createScannerResponse(&scanResponseModel.StatusDetails[i]))
 		}
 	}
