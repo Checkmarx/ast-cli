6b327f99e6e0c89e3d23724d5c49bc3bf52524be
tamarleviCm <tamar.levi@checkmarx.com>
2024-05-26 11:04:02 +0300

    executable file

--- internal/commands/scarealtime/sca-linux.go
+++ internal/commands/scarealtime/sca-linux.go
@@ -7,10 +7,10 @@ import (
 )
 
 var Params = osinstaller.InstallableRealTime{
-	ExecutableFilePath: "ScaResolver",
-	DownloadURL:        "https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-linux64.tar.gz",
-	HashDownloadURL:    "https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-linux64.tar.gz.sha256sum",
-	FileName:           "ScaResolver.tar.gz",
-	HashFileName:       "ScaResolver.tar.gz.sha256sum",
-	WorkingDirName:     "SCARealtime",
+	ExecutableFile:  "ScaResolver",
+	DownloadURL:     "https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-linux64.tar.gz",
+	HashDownloadURL: "https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-linux64.tar.gz.sha256sum",
+	FileName:        "ScaResolver.tar.gz",
+	HashFileName:    "ScaResolver.tar.gz.sha256sum",
+	WorkingDirName:  "SCARealtime",
 }
--- internal/commands/scarealtime/sca-windows.go
+++ internal/commands/scarealtime/sca-windows.go
@@ -7,10 +7,10 @@ import (
 )
 
 var Params = osinstaller.InstallableRealTime{
-	ExecutableFilePath: "ScaResolver.exe",
-	DownloadURL:        "https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-win64.zip",
-	HashDownloadURL:    "https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-win64.zip.sha256sum",
-	FileName:           "ScaResolver.zip",
-	HashFileName:       "ScaResolver.zip.sha256sum",
-	WorkingDirName:     "SCARealtime",
+	ExecutableFile:  "ScaResolver.exe",
+	DownloadURL:     "https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-win64.zip",
+	HashDownloadURL: "https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-win64.zip.sha256sum",
+	FileName:        "ScaResolver.zip",
+	HashFileName:    "ScaResolver.zip.sha256sum",
+	WorkingDirName:  "SCARealtime",
 }
