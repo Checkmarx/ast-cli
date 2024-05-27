1e7b003890935f51feb1dd7045ef6e1e6aac223f
AlvoBen <alvo@post.bgu.ac.il>
2024-05-27 10:31:02 +0300

    Exclude microsastengine folder from integration coverage

--- internal/commands/.scripts/integration_up.sh
+++ internal/commands/.scripts/integration_up.sh
@@ -10,11 +10,31 @@ wget https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-linux64.tar.g
 tar -xzvf ScaResolver-linux64.tar.gz -C /tmp
 rm -rf ScaResolver-linux64.tar.gz
 
+INCLUDE_PACKAGES=(
+  "github.com/checkmarx/ast-cli/internal/commands"
+  "github.com/checkmarx/ast-cli/internal/services"
+  "github.com/checkmarx/ast-cli/internal/wrappers"
+)
+
+EXCLUDE_PACKAGE="github.com/checkmarx/ast-cli/internal/wrappers/microsastengine"
+
+COVERPKG=""
+for pkg in "${INCLUDE_PACKAGES[@]}"; do
+  for subpkg in $(go list ${pkg}/...); do
+    if [[ ${subpkg} != ${EXCLUDE_PACKAGE}* ]]; then
+      COVERPKG="${COVERPKG},${subpkg}"
+    fi
+  done
+done
+
+# Remove leading comma
+COVERPKG=${COVERPKG:1}
+
 go test \
   -tags integration \
   -v \
   -timeout 210m \
-  -coverpkg github.com/checkmarx/ast-cli/internal/commands,github.com/checkmarx/ast-cli/internal/services,github.com/checkmarx/ast-cli/internal/wrappers,github.com/checkmarx/ast-cli/internal/microsastengine \
+  -coverpkg "$COVERPKG" \
   -coverprofile cover.out \
   github.com/checkmarx/ast-cli/test/integration
 
