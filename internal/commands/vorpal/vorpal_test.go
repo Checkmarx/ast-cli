a60ba08273a6a88fa5928f534f1926adf41d8dc7
tamarleviCm <tamar.levi@checkmarx.com>
2024-05-28 21:22:59 +0300

    tests

--- internal/commands/vorpal/vorpal_test.go
+++ internal/commands/vorpal/vorpal_test.go
@@ -10,41 +10,42 @@ import (
 )
 
 func TestInstallOrUpgrade_firstInstallation_Success(t *testing.T) {
-	os.RemoveAll(vorpalconfiguration.Params.WorkingDir())
-	err := osinstaller.InstallOrUpgrade(&vorpalconfiguration.Params)
-	assert.NilError(t, err)
-	dirEntry, err := os.ReadDir(vorpalconfiguration.Params.WorkingDir())
-	assert.NilError(t, err, "Error reading directory ", vorpalconfiguration.Params.WorkingDir())
-	assert.Assert(t, dirContainsFile(dirEntry, vorpalconfiguration.Params.ExecutableFile))
-	assert.Assert(t, dirContainsFile(dirEntry, vorpalconfiguration.Params.HashFileName))
+	err := firstInstallation()
+	assert.NilError(t, err, "Error on first installation of vorpal")
+	checkFiles(t)
 }
 
-func dirContainsFile(entry []os.DirEntry, file string) bool {
-	for _, e := range entry {
-		if e.Name() == file {
-			return true
-		}
-	}
-	return false
+func checkFiles(t *testing.T) {
+	fileExists, _ := osinstaller.FileExists(vorpalconfiguration.Params.ExecutableFilePath())
+	assert.Assert(t, fileExists, "Executable file not found")
+	fileExists, _ = osinstaller.FileExists(vorpalconfiguration.Params.HashFilePath())
+	assert.Assert(t, fileExists, "Hash file not found")
+}
+
+func firstInstallation() error {
+	os.RemoveAll(vorpalconfiguration.Params.WorkingDir())
+	err := osinstaller.InstallOrUpgrade(&vorpalconfiguration.Params)
+	return err
 }
 
 func TestInstallOrUpgrade_installationIsUpToDate_Success(t *testing.T) {
-	err := osinstaller.InstallOrUpgrade(&vorpalconfiguration.Params)
-	assert.NilError(t, err)
+	err := firstInstallation()
+	assert.NilError(t, err, "Error on first installation of vorpal")
+	err = osinstaller.InstallOrUpgrade(&vorpalconfiguration.Params)
+	assert.NilError(t, err, "Error when not need to upgrade")
 }
 
 func TestInstallOrUpgrade_installationIsNotUpToDate_Success(t *testing.T) {
+	err := firstInstallation()
+	assert.NilError(t, err, "Error on first installation of vorpal")
 	changeHashFile()
-	err := osinstaller.InstallOrUpgrade(&vorpalconfiguration.Params)
-	assert.NilError(t, err)
-	dirEntry, err := os.ReadDir(vorpalconfiguration.Params.WorkingDir())
-	assert.NilError(t, err, "Error reading directory ", vorpalconfiguration.Params.WorkingDir())
-	assert.Assert(t, dirContainsFile(dirEntry, vorpalconfiguration.Params.ExecutableFile))
-	assert.Assert(t, dirContainsFile(dirEntry, vorpalconfiguration.Params.HashFileName))
+	err = osinstaller.InstallOrUpgrade(&vorpalconfiguration.Params)
+	assert.NilError(t, err, "Error when need to upgrade")
+	checkFiles(t)
 }
 
 func changeHashFile() {
 	content, _ := os.ReadFile(vorpalconfiguration.Params.HashFilePath())
 	content[0]++
-	os.WriteFile(vorpalconfiguration.Params.HashFilePath(), content, os.ModePerm)
+	_ = os.WriteFile(vorpalconfiguration.Params.HashFilePath(), content, os.ModePerm)
 }
