14a3e47f58e671723ffcd9051fe8740e8e2a3c5d
elchananarb <elchanan.arbiv@checkmarx.com>
2024-06-17 10:34:19 +0300

    v2

--- test/integration/project_test.go
+++ test/integration/project_test.go
@@ -2,73 +2,144 @@
 
 package integration
 
-//
-//import (
-//	"fmt"
-//	"io"
-//	"io/ioutil"
-//	"log"
-//	"os"
-//	"strings"
-//	"testing"
-//
-//	"github.com/checkmarx/ast-cli/internal/commands/util/printer"
-//	errorConstants "github.com/checkmarx/ast-cli/internal/constants/errors"
-//	"github.com/checkmarx/ast-cli/internal/params"
-//	"github.com/checkmarx/ast-cli/internal/wrappers"
-//	"github.com/google/uuid"
-//	"github.com/spf13/viper"
-//
-//	"gotest.tools/assert"
-//)
-//
-//const SSHKeyFilePath = "ssh-key-file.txt"
-//
-//// End-to-end test of project handling.
-//// - Create a project
-//// - Get and assert the project exists
-//// - Get all tags
-//// - Assert assigned tags exist
-//// - Assert project contains the tags
-//// - Assert project contains the groups
-//// - Delete the created project
-//// - Get and assert the project was deleted
-//func TestProjectsE2E(t *testing.T) {
-//	projectID, _ := createProject(t, Tags, Groups)
-//
-//	response := listProjectByID(t, projectID)
-//
-//	assert.Equal(t, len(response), 1, "Total projects should be 1")
-//	assert.Equal(t, response[0].ID, projectID, "Project ID should match the created project")
-//
-//	project := showProject(t, projectID)
-//	assert.Equal(t, project.ID, projectID, "Project ID should match the created project")
-//
-//	assertTagsAndGroups(t, project, Groups)
-//
-//	deleteProject(t, projectID)
-//
-//	response = listProjectByID(t, projectID)
-//
-//	assert.Equal(t, len(response), 0, "Total projects should be 0 as the project was deleted")
-//}
-//
-//// Assert project contains created tags and groups
-//func assertTagsAndGroups(t *testing.T, project wrappers.ProjectResponseModel, groups []string) {
-//
-//	allTags := getAllTags(t, "project")
-//
-//	for key := range Tags {
-//		_, ok := allTags[key]
-//		assert.Assert(t, ok, "Get all tags response should contain all created tags. Missing %s", key)
-//
-//		val, ok := project.Tags[key]
-//		assert.Assert(t, ok, "Project should contain all created tags. Missing %s", key)
-//		assert.Equal(t, val, Tags[key], "Tag value should be equal")
-//	}
-//
-//	assert.Assert(t, len(project.Groups) >= len(groups), "The project must contain at least %d groups", len(groups))
-//}
+import (
+	"fmt"
+	"testing"
+
+	"github.com/checkmarx/ast-cli/internal/commands/util/printer"
+	"github.com/checkmarx/ast-cli/internal/params"
+	"github.com/checkmarx/ast-cli/internal/wrappers"
+	"github.com/spf13/viper"
+
+	"gotest.tools/assert"
+)
+
+const SSHKeyFilePath = "ssh-key-file.txt"
+
+func createProject(t *testing.T, tags map[string]string, groups []string) (string, string) {
+	projectName := getProjectNameForTest() + "_for_project"
+	tagsStr := formatTags(tags)
+	groupsStr := formatGroups(groups)
+
+	fmt.Printf("Creating project : %s \n", projectName)
+	outBuffer := executeCmdNilAssertion(
+		t, "Creating a project should pass",
+		"project", "create",
+		flag(params.FormatFlag), printer.FormatJSON,
+		flag(params.ProjectName), projectName,
+		flag(params.BranchFlag), "master",
+		flag(params.TagList), tagsStr,
+		flag(params.GroupList), groupsStr,
+	)
+
+	createdProject := wrappers.ProjectResponseModel{}
+	createdProjectJSON := unmarshall(t, outBuffer, &createdProject, "Reading project create response JSON should pass")
+
+	fmt.Println("Response after project is created : ", string(createdProjectJSON))
+	fmt.Printf("New project created with id: %s \n", createdProject.ID)
+
+	return createdProject.ID, projectName
+}
+
+func deleteProject(t *testing.T, projectID string) {
+	log.Println("Deleting the project with id ", projectID)
+	fmt.Println("Deleting the project with id ", projectID)
+	executeCmdNilAssertion(
+		t,
+		"Deleting a project should pass",
+		"project",
+		"delete",
+		flag(params.ProjectIDFlag),
+		projectID,
+	)
+}
+
+func deleteProjectByName(t *testing.T, projectName string) {
+	projectsWrapper := wrappers.NewHTTPProjectsWrapper(viper.GetString(params.ProjectsPathKey))
+	projectModel, _, err := projectsWrapper.GetByName(projectName)
+	if err == nil && projectModel != nil {
+		deleteProject(t, projectModel.ID)
+	}
+}
+
+func listProjectByID(t *testing.T, projectID string) []wrappers.ProjectResponseModel {
+	idFilter := fmt.Sprintf("ids=%s", projectID)
+	fmt.Println("Listing project for id ", projectID)
+	outputBuffer := executeCmdNilAssertion(
+		t,
+		"Getting the project should pass",
+		"project", "list",
+		flag(params.FormatFlag), printer.FormatJSON, flag(params.FilterFlag), idFilter,
+	)
+	fmt.Println("Listing project for id output buffer", outputBuffer)
+	var projects []wrappers.ProjectResponseModel
+	_ = unmarshall(t, outputBuffer, &projects, "Reading all projects response JSON should pass")
+	fmt.Println("Listing project for id projects length: ", len(projects))
+
+	return projects
+}
+
+func showProject(t *testing.T, projectID string) wrappers.ProjectResponseModel {
+	assertRequiredParameter(t, "Failed getting a project: Please provide a project ID", "project", "show")
+
+	outputBuffer := executeCmdNilAssertion(
+		t, "Getting the project should pass", "project", "show",
+		flag(params.FormatFlag), printer.FormatJSON,
+		flag(params.ProjectIDFlag), projectID,
+	)
+
+	var project wrappers.ProjectResponseModel
+	_ = unmarshall(t, outputBuffer, &project, "Reading project JSON should pass")
+
+	return project
+}
+
+// End-to-end test of project handling.
+// - Create a project
+// - Get and assert the project exists
+// - Get all tags
+// - Assert assigned tags exist
+// - Assert project contains the tags
+// - Assert project contains the groups
+// - Delete the created project
+// - Get and assert the project was deleted
+func TestProjectsE2E(t *testing.T) {
+	projectID, _ := createProject(t, Tags, Groups)
+
+	response := listProjectByID(t, projectID)
+
+	assert.Equal(t, len(response), 1, "Total projects should be 1")
+	assert.Equal(t, response[0].ID, projectID, "Project ID should match the created project")
+
+	project := showProject(t, projectID)
+	assert.Equal(t, project.ID, projectID, "Project ID should match the created project")
+
+	assertTagsAndGroups(t, project, Groups)
+
+	deleteProject(t, projectID)
+
+	response = listProjectByID(t, projectID)
+
+	assert.Equal(t, len(response), 0, "Total projects should be 0 as the project was deleted")
+}
+
+// Assert project contains created tags and groups
+func assertTagsAndGroups(t *testing.T, project wrappers.ProjectResponseModel, groups []string) {
+
+	allTags := getAllTags(t, "project")
+
+	for key := range Tags {
+		_, ok := allTags[key]
+		assert.Assert(t, ok, "Get all tags response should contain all created tags. Missing %s", key)
+
+		val, ok := project.Tags[key]
+		assert.Assert(t, ok, "Project should contain all created tags. Missing %s", key)
+		assert.Equal(t, val, Tags[key], "Tag value should be equal")
+	}
+
+	assert.Assert(t, len(project.Groups) >= len(groups), "The project must contain at least %d groups", len(groups))
+}
+
 //
 //// Create a project with empty project name should fail
 //func TestCreateEmptyProjectName(t *testing.T) {
@@ -156,83 +227,6 @@ package integration
 //	assert.Assert(t, strings.Contains(string(result), "[]"))
 //}
 //
-//func createProject(t *testing.T, tags map[string]string, groups []string) (string, string) {
-//	projectName := getProjectNameForTest() + "_for_project"
-//	tagsStr := formatTags(tags)
-//	groupsStr := formatGroups(groups)
-//
-//	fmt.Printf("Creating project : %s \n", projectName)
-//	outBuffer := executeCmdNilAssertion(
-//		t, "Creating a project should pass",
-//		"project", "create",
-//		flag(params.FormatFlag), printer.FormatJSON,
-//		flag(params.ProjectName), projectName,
-//		flag(params.BranchFlag), "master",
-//		flag(params.TagList), tagsStr,
-//		flag(params.GroupList), groupsStr,
-//	)
-//
-//	createdProject := wrappers.ProjectResponseModel{}
-//	createdProjectJSON := unmarshall(t, outBuffer, &createdProject, "Reading project create response JSON should pass")
-//
-//	fmt.Println("Response after project is created : ", string(createdProjectJSON))
-//	fmt.Printf("New project created with id: %s \n", createdProject.ID)
-//
-//	return createdProject.ID, projectName
-//}
-//
-//func deleteProject(t *testing.T, projectID string) {
-//	log.Println("Deleting the project with id ", projectID)
-//	fmt.Println("Deleting the project with id ", projectID)
-//	executeCmdNilAssertion(
-//		t,
-//		"Deleting a project should pass",
-//		"project",
-//		"delete",
-//		flag(params.ProjectIDFlag),
-//		projectID,
-//	)
-//}
-//
-//func deleteProjectByName(t *testing.T, projectName string) {
-//	projectsWrapper := wrappers.NewHTTPProjectsWrapper(viper.GetString(params.ProjectsPathKey))
-//	projectModel, _, err := projectsWrapper.GetByName(projectName)
-//	if err == nil && projectModel != nil {
-//		deleteProject(t, projectModel.ID)
-//	}
-//}
-//
-//func listProjectByID(t *testing.T, projectID string) []wrappers.ProjectResponseModel {
-//	idFilter := fmt.Sprintf("ids=%s", projectID)
-//	fmt.Println("Listing project for id ", projectID)
-//	outputBuffer := executeCmdNilAssertion(
-//		t,
-//		"Getting the project should pass",
-//		"project", "list",
-//		flag(params.FormatFlag), printer.FormatJSON, flag(params.FilterFlag), idFilter,
-//	)
-//	fmt.Println("Listing project for id output buffer", outputBuffer)
-//	var projects []wrappers.ProjectResponseModel
-//	_ = unmarshall(t, outputBuffer, &projects, "Reading all projects response JSON should pass")
-//	fmt.Println("Listing project for id projects length: ", len(projects))
-//
-//	return projects
-//}
-//
-//func showProject(t *testing.T, projectID string) wrappers.ProjectResponseModel {
-//	assertRequiredParameter(t, "Failed getting a project: Please provide a project ID", "project", "show")
-//
-//	outputBuffer := executeCmdNilAssertion(
-//		t, "Getting the project should pass", "project", "show",
-//		flag(params.FormatFlag), printer.FormatJSON,
-//		flag(params.ProjectIDFlag), projectID,
-//	)
-//
-//	var project wrappers.ProjectResponseModel
-//	_ = unmarshall(t, outputBuffer, &project, "Reading project JSON should pass")
-//
-//	return project
-//}
 //
 //func TestCreateProjectWithSSHKey(t *testing.T) {
 //	projectName := fmt.Sprintf("ast-cli-tests_%s", uuid.New().String()) + "_for_project"
