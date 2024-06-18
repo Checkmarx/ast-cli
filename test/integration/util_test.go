1e74c54dd8ae3d0b5a540ea8a29c38869dcfdb81
elchananarb <elchanan.arbiv@checkmarx.com>
2024-06-18 12:53:28 +0300

    del tests that not necessary to flow

--- test/integration/util_test.go
+++ test/integration/util_test.go
@@ -2,36 +2,30 @@
 
 package integration
 
-import (
-	"testing"
-
-	"github.com/checkmarx/ast-cli/internal/params"
-)
-
 const (
 	maskCommand           = "mask"
 	resultsFileValue      = "data/package.json"
 	resultFileNonExisting = "data/package.jso"
 )
 
-func TestMaskSecrets(t *testing.T) {
-	executeCmdNilAssertion(
-		t,
-		"Remediating kics result",
-		utilsCommand,
-		maskCommand,
-		flag(params.ChatKicsResultFile),
-		resultsFileValue,
-	)
-}
-
-func TestFailedMaskSecrets(t *testing.T) {
-	args := []string{
-		utilsCommand,
-		maskCommand,
-		flag(params.ChatKicsResultFile),
-		resultFileNonExisting,
-	}
-	err, _ := executeCommand(t, args...)
-	assertError(t, err, "Error opening file : open data/package.jso: no such file or directory")
-}
+//func TestMaskSecrets(t *testing.T) {
+//	executeCmdNilAssertion(
+//		t,
+//		"Remediating kics result",
+//		utilsCommand,
+//		maskCommand,
+//		flag(params.ChatKicsResultFile),
+//		resultsFileValue,
+//	)
+//}
+//
+//func TestFailedMaskSecrets(t *testing.T) {
+//	args := []string{
+//		utilsCommand,
+//		maskCommand,
+//		flag(params.ChatKicsResultFile),
+//		resultFileNonExisting,
+//	}
+//	err, _ := executeCommand(t, args...)
+//	assertError(t, err, "Error opening file : open data/package.jso: no such file or directory")
+//}
