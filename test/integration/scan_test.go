aaaf89824cd75c40f4509554a9bc8b85eb1113d2
AlvoBen <144705560+AlvoBen@users.noreply.github.com>
2024-06-27 16:44:00 +0300

    CLI | Add check to symbolicLink value (AST-45529) (#786)
    * add symbolincLink value check
    
    * refactor addDirFiles
    
    * add test files
    
    ---------
    
    Co-authored-by: AlvoBen <alvo@post.bgu.ac.il>

--- test/integration/scan_test.go
+++ test/integration/scan_test.go
@@ -300,6 +300,19 @@ func TestScanCreate_ExistingApplicationAndExistingProject_CreateScanSuccessfully
 	assert.NilError(t, err)
 }
 
+func TestScanCreate_FolderWithSymbolicLink_CreateScanSuccessfully(t *testing.T) {
+	args := []string{
+		"scan", "create",
+		flag(params.ProjectName), "my-project",
+		flag(params.SourcesFlag), "data/project-with-directory-symlink",
+		flag(params.ScanTypes), "iac-security",
+		flag(params.BranchFlag), "dummy_branch",
+	}
+
+	err, _ := executeCommand(t, args...)
+	assert.NilError(t, err)
+}
+
 func TestScanCreate_ExistingApplicationAndNotExistingProject_CreatingNewProjectAndCreateScanSuccessfully(t *testing.T) {
 	args := []string{
 		"scan", "create",
