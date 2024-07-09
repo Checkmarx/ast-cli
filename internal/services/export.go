dfa77bd82fead2cf509aa3c8ccc40caeae15f92c
AlvoBen <alvo@post.bgu.ac.il>
2024-07-09 18:14:45 +0300

    remove sbom wrapper and add it's logic to export wrapper

--- internal/services/export.go
+++ internal/services/export.go
@@ -1,1 +1,77 @@
 package services
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/checkmarx/ast-cli/internal/logger"
+	"github.com/checkmarx/ast-cli/internal/wrappers"
+	"github.com/pkg/errors"
+)
+
+const defaultSbomOption = "CycloneDxJson"
+
+func GetExportPackage(exportWrapper wrappers.ExportWrapper, scanID string) (*wrappers.ScaPackageCollectionExport, error) {
+	exportID, err := exportWrapper.InitiateExportRequest(scanID, "ScanReportJson")
+	if err != nil {
+		return nil, err
+	}
+	logger.PrintIfVerbose(fmt.Sprintf("Initiated export request for scanID %s. ExportID: %s", scanID, exportID))
+
+	fileURL, err := exportWrapper.CheckExportStatus(exportID)
+	if err != nil {
+		return nil, err
+	}
+	logger.PrintIfVerbose(fmt.Sprintf("Checked export status. File URL: %s", fileURL))
+
+	scaPackageCollection, err := exportWrapper.GetScaPackageCollectionExport(fileURL)
+	if err != nil {
+		return nil, err
+	}
+	logger.PrintIfVerbose("Retrieved SCA package collection export successfully")
+
+	return scaPackageCollection, nil
+}
+
+func ExportSbomResults(exportWrapper wrappers.ExportWrapper,
+	targetFile string,
+	results *wrappers.ResultSummary,
+	formatSbomOptions string) error {
+	fileFormat := defaultSbomOption
+	if formatSbomOptions != "" && formatSbomOptions != defaultSbomOption {
+		format, err := validateSbomOptions(formatSbomOptions)
+		if err != nil {
+			return err
+		}
+		fileFormat = format
+	}
+
+	exportID, err := exportWrapper.InitiateExportRequest(results.ScanID, fileFormat)
+	if err != nil {
+		return err
+	}
+
+	reportURL, err := exportWrapper.CheckExportStatus(exportID)
+	if err != nil {
+		return err
+	}
+
+	err = exportWrapper.DownloadExportReport(reportURL, targetFile)
+	if err != nil {
+		return errors.Wrapf(err, "%s", "Failed downloading SBOM report")
+	}
+	return nil
+}
+
+func validateSbomOptions(sbomOption string) (string, error) {
+	var sbomOptionsStringMap = map[string]string{
+		"cyclonedxjson": "CycloneDxJson",
+		"cyclonedxxml":  "CycloneDxXml",
+		"spdxjson":      "SpdxJson",
+	}
+	sbomOption = strings.ToLower(strings.ReplaceAll(sbomOption, " ", ""))
+	if sbomOptionsStringMap[sbomOption] != "" {
+		return sbomOptionsStringMap[sbomOption], nil
+	}
+	return "", errors.Errorf("invalid SBOM option: %s", sbomOption)
+}
