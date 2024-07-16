71e3ec4b4abe28fc8d817168c89e14f9bebbe872
AlvoBen <144705560+AlvoBen@users.noreply.github.com>
2024-07-16 14:49:28 +0300

    CLI | Change Vorpal vorpal-latest-version flag flow (AST-48376) (#797)
    * change vorpal-latest-version flow
    
    * change vorpal-latest-version flow
    
    * resolve conversations
    
    * add bool type to osinstaller to improve readability
    
    * added unit tests
    
    * added unit tests
    
    * change the port configuration
    
    * fix unit tests
    
    ---------
    
    Co-authored-by: AlvoBen <alvo@post.bgu.ac.il>
    Co-authored-by: tamarleviCm <tamar.levi@checkmarx.com>

--- internal/commands/scarealtime/sca-realtime.go
+++ internal/commands/scarealtime/sca-realtime.go
@@ -75,7 +75,7 @@ func RunScaRealtime(scaRealTimeWrapper wrappers.ScaRealTimeWrapper) func(*cobra.
 		fmt.Println("Running SCA Realtime...")
 
 		// Handle SCA Resolver. Checks if it already exists and if it is in the latest version
-		err = osinstaller.InstallOrUpgrade(&scaconfig.Params)
+		_, err = osinstaller.InstallOrUpgrade(&scaconfig.Params)
 		if err != nil {
 			return err
 		}
