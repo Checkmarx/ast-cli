c7327ac80d58fcd1cae0a457604bbd4778b430a6
tiagobcx <tiago.baptista@checkmarx.com>
2024-08-01 15:12:38 +0100

    changing ssh repo

--- test/integration/root_test.go
+++ test/integration/root_test.go
@@ -20,7 +20,7 @@ const (
 	Dir                   = "./data"
 	Zip                   = "data/sources.zip"
 	SlowRepo              = "https://github.com/WebGoat/WebGoat"
-	SSHRepo               = "git@github.com:hmmachadocx/hmmachado_dummy_project.git"
+	SSHRepo               = "git@github.com:Yoavast/CX-AST.git"
 	SlowRepoBranch        = "develop"
 	resolverEnvVar        = "SCA_RESOLVER"
 	resolverEnvVarDefault = "./ScaResolver"
