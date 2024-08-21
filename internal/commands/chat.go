c44738d8b19b28460272c29b81292777c931dbf3
avisab-cx <Avi.Sabzerou@checkmarx.com>
2024-08-21 17:07:20 +0300

    Update configuration service field names

--- internal/commands/chat.go
+++ internal/commands/chat.go
@@ -8,11 +8,11 @@ import (
 const (
 	ConversationIDErrorFormat           = "Invalid conversation ID %s"
 	AiGuidedRemediationEnabled          = "scan.config.plugins.aiGuidedRemediation"
-	AiGuidedRemediationEngine           = "scan.config.plugins.aiGuidedRemediation.aiEngine" // todo: check final value
+	AiGuidedRemediationEngine           = "scan.config.plugins.aiGuidedRemediationAiEngine" // todo: check final value
 	AiGuidedRemediationOpenAiValue      = "openai"
 	AiGuidedRemediationAzureAiValue     = "azureai"
 	AiGuidedRemediationCheckmarxAiValue = "checkmarxai"
-	AzureAiModel                        = "scan.config.plugins.aiGuidedRemediation.azureAi.model" // todo: check final value
+	AzureAiModel                        = "scan.config.plugins.aiGuidedRemediationAzureAiModel" // todo: check final value
 )
 
 func NewChatCommand(chatWrapper wrappers.ChatWrapper, tenantWrapper wrappers.TenantConfigurationWrapper) *cobra.Command {
