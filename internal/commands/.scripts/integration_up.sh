839497bba460d6bb4f583d36eb1e9837229885f8
AlvoBen <alvo@post.bgu.ac.il>
2024-08-22 09:41:10 +0300

    failed the job if some test failed

--- internal/commands/.scripts/integration_up.sh
+++ internal/commands/.scripts/integration_up.sh
@@ -14,6 +14,7 @@ go test \
   -tags integration \
   -v \
   -timeout 210m \
+  -failfast \
   -coverpkg github.com/checkmarx/ast-cli/internal/commands,github.com/checkmarx/ast-cli/internal/services,github.com/checkmarx/ast-cli/internal/wrappers \
   -coverprofile cover.out \
   github.com/checkmarx/ast-cli/test/integration
