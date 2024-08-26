91a9f22896199756aed79045956ed31a752eabc7
AlvoBen <alvo@post.bgu.ac.il>
2024-08-26 15:01:37 +0300

    Fix export service call param and update vulnerability identifier

--- internal/commands/result.go
+++ internal/commands/result.go
@@ -2317,7 +2317,8 @@ func buildAuxiliaryScaMaps(resultsModel *wrappers.ScanResultsCollection, scaPack
 				locationsByID[packages.ID] = currentPackage.Locations
 			}
 			for _, types := range *scaTypeModel {
-				typesByCVE[types.ID] = types
+				identifier := fmt.Sprintf("%s:%s", types.ID, types.PackageID)
+				typesByCVE[identifier] = types
 			}
 		}
 	}
@@ -2325,7 +2326,8 @@ func buildAuxiliaryScaMaps(resultsModel *wrappers.ScanResultsCollection, scaPack
 }
 
 func buildScaType(typesByCVE map[string]wrappers.ScaTypeCollection, result *wrappers.ScanResult) string {
-	types, ok := typesByCVE[result.ID]
+	identifier := buildVulnerabilityIdentifier(result)
+	types, ok := typesByCVE[identifier]
 	if ok && types.Type == "SupplyChain" {
 		return "Supply Chain"
 	}
@@ -2333,15 +2335,16 @@ func buildScaType(typesByCVE map[string]wrappers.ScaTypeCollection, result *wrap
 }
 
 func buildScaState(typesByCVE map[string]wrappers.ScaTypeCollection, result *wrappers.ScanResult) string {
-	types, ok := typesByCVE[result.ID]
-	if ok && (types.IsIgnored || isSnoozeOrMutePackage(&types)) {
+	identifier := buildVulnerabilityIdentifier(result)
+	types, ok := typesByCVE[identifier]
+	if ok && types.IsIgnored {
 		return notExploitable
 	}
 	return result.State
 }
 
-func isSnoozeOrMutePackage(result *wrappers.ScaTypeCollection) bool {
-	return strings.EqualFold(result.RiskState, snoozeLabel) || strings.EqualFold(result.RiskState, muteLabel)
+func buildVulnerabilityIdentifier(result *wrappers.ScanResult) string {
+	return fmt.Sprintf("%s:%s", result.ID, result.ScanResultData.PackageIdentifier)
 }
 
 func addPackageInformation(
