257c869c6dd143940d4e886bfab7e39c7296dd04
AlvoBen <alvo@post.bgu.ac.il>
2024-08-27 12:42:32 +0300

    update integration_up.sh

--- internal/commands/.scripts/integration_up.sh
+++ internal/commands/.scripts/integration_up.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+docker run \
+  --name squid \
+  -d \
+  -p $PROXY_PORT:3128 \
+  -v $(pwd)/internal/commands/.scripts/squid/squid.conf:/etc/squid/squid.conf \
+  -v $(pwd)/internal/commands/.scripts/squid/passwords:/etc/squid/passwords \
+  ubuntu/squid:5.2-22.04_beta
 
+wget https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-linux64.tar.gz
+tar -xzvf ScaResolver-linux64.tar.gz -C /tmp
+rm -rf ScaResolver-linux64.tar.gz
 # Step 1: Check if the failedTests file exists
 FAILED_TESTS_FILE="failedTests"
 
