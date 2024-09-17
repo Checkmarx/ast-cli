17a3f870d13400905cb942758fca4a7d731bc6e1
miryamfoiferCX <miryam.foifer@checkmarx.com>
2024-09-17 15:04:13 +0300

    adding fix of scs for running tests

--- internal/commands/result.go
+++ internal/commands/result.go
@@ -1167,6 +1167,10 @@ func countResult(summary *wrappers.ResultSummary, result *wrappers.ScanResult) {
 		case infoLabel:
 			summary.InfoIssues++
 		}
+
+		if strings.HasPrefix(engineType, "sscs") {
+			engineType = commonParams.ScsType
+		}
 		summary.UpdateEngineResultSummary(engineType, severity)
 	}
 }
