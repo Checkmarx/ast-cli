4d5ccc5853e8ba611d8f66b35acc36c5f9d10bbe
galactica <tiago.baptista@checkmarx.com>
2024-11-13 23:10:30 +0200

    fix lint

--- internal/commands/util/pr_test.go
+++ internal/commands/util/pr_test.go
@@ -164,6 +164,7 @@ func TestRepoSlugFormatBB(t *testing.T) {
 	}
 
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			slug := repoSlugFormatBB(tt.repoNameFlag)
 			asserts.Equal(t, tt.expectedSlug, slug)
