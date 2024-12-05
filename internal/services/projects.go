16faf599ff368ebfb281e64883db6ce3541f5dc7
miryamfoiferCX <miryam.foifer@checkmarx.com>
2024-12-05 09:02:44 +0200

    move code to applications.go service

--- internal/services/projects.go
+++ internal/services/projects.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 	"time"
 
-	errorConstants "github.com/checkmarx/ast-cli/internal/constants/errors"
 	"github.com/checkmarx/ast-cli/internal/logger"
 	commonParams "github.com/checkmarx/ast-cli/internal/params"
 	"github.com/checkmarx/ast-cli/internal/wrappers"
@@ -91,49 +90,6 @@ func GetProjectsCollectionByProjectName(projectName string, projectsWrapper wrap
 	}
 
 	return resp, nil
-}
-
-func getApplicationID(applicationName string, applicationsWrapper wrappers.ApplicationsWrapper) ([]string, error) {
-	var applicationID []string
-	if applicationName != "" {
-		application, getAppErr := GetApplication(applicationName, applicationsWrapper)
-		if getAppErr != nil {
-			return nil, getAppErr
-		}
-		if application == nil {
-			return nil, errors.Errorf(errorConstants.ApplicationDoesntExistOrNoPermission)
-		}
-		applicationID = []string{application.ID}
-	}
-	return applicationID, nil
-}
-
-func GetApplication(applicationName string, applicationsWrapper wrappers.ApplicationsWrapper) (*wrappers.Application, error) {
-	if applicationName != "" {
-		params := make(map[string]string)
-		params["name"] = applicationName
-		resp, err := applicationsWrapper.Get(params)
-		if err != nil {
-			return nil, err
-		}
-		if resp.Applications != nil && len(resp.Applications) > 0 {
-			application := verifyApplicationNameExactMatch(applicationName, resp)
-
-			return application, nil
-		}
-	}
-	return nil, nil
-}
-
-func verifyApplicationNameExactMatch(applicationName string, resp *wrappers.ApplicationsResponseModel) *wrappers.Application {
-	var application *wrappers.Application
-	for i := range resp.Applications {
-		if resp.Applications[i].Name == applicationName {
-			application = &resp.Applications[i]
-			break
-		}
-	}
-	return application
 }
 
 func createProject(
