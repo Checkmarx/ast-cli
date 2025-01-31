50a9f90b14e8883c37ca8b1509b5b803626896e3
Andre Macedo <149069722+amacedoo@users.noreply.github.com>
2025-01-31 14:32:54 +0000

    Test

--- internal/commands/scan.go
+++ internal/commands/scan.go
@@ -60,7 +60,7 @@ const (
 	containerVolumeFlag             = "-v"
 	containerNameFlag               = "--name"
 	containerRemove                 = "--rm"
-	containerImage                  = "checkmarx/kics:v2.1.3"
+	containerImage                  = "ruigomes99/kics:latest"
 	containerScan                   = "scan"
 	containerScanPathFlag           = "-p"
 	containerScanPath               = "/path"
