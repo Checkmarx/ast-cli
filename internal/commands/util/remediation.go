129d9f5d8e62cd42bbc92477591fcc76e09d4e63
AlvoBen <alvo@post.bgu.ac.il>
2025-02-11 12:10:16 +0200

    update kics realtime imgae tag to 2.1.5

--- internal/commands/util/remediation.go
+++ internal/commands/util/remediation.go
@@ -27,7 +27,7 @@ const (
 	filesContainerVolume      = ":/files"
 	resultsContainerLocation  = "/kics/"
 	containerRemove           = "--rm"
-	containerImage            = "checkmarx/kics:v2.1.3"
+	containerImage            = "checkmarx/kics:v2.1.5"
 	containerNameFlag         = "--name"
 	remediateCommand          = "remediate"
 	resultsFlag               = "--results"
