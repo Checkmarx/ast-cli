c83186d4ede75ff6b3fe954c482a90738ec60e0c
Itay Paz <Itay.paz@checkmarx.com>
2025-03-09 15:23:32 +0200

    add integration test for triage update and show with custom states

--- test/integration/predicate_test.go
+++ test/integration/predicate_test.go
@@ -152,3 +152,89 @@ func TestPredicateWithInvalidValues(t *testing.T) {
 	_ = unmarshall(t, responseKics, &kicsPredicate, "Reading predicate should pass")
 	assert.Assert(t, kicsPredicate.TotalCount == 0, "Predicate with invalid values should have 0 as the result.")
 }
+
+
+
+func TestSastUpdateAndGetPredicateWithCustomStateID(t *testing.T) {
+	t.Skip("Skipping TestSastUpdateAndGetPredicateWithCustomStateID for now")
+
+	fmt.Println("Step 1: Testing the command 'triage update' with custom state-id to update an issue from the project.")
+
+	scanID, projectID := getRootScan(t)
+	_ = executeCmdNilAssertion(
+		t, "Results show generating JSON report with options should pass",
+		"results", "show",
+		flag(params.ScanIDFlag), scanID, flag(params.TargetFormatFlag), printer.FormatJSON,
+		flag(params.TargetPathFlag), resultsDirectory,
+		flag(params.TargetFlag), fileName,
+	)
+
+	defer func() {
+		_ = os.RemoveAll(fmt.Sprintf(resultsDirectory))
+	}()
+
+	result := wrappers.ScanResultsCollection{}
+
+	_, err := os.Stat(fmt.Sprintf("%s%s.%s", resultsDirectory, fileName, printer.FormatJSON))
+	assert.NilError(t, err, "Report file should exist for extension "+printer.FormatJSON)
+
+	file, err := os.ReadFile(fmt.Sprintf("%s%s.%s", resultsDirectory, fileName, printer.FormatJSON))
+	assert.NilError(t, err, "error reading file")
+
+	err = json.Unmarshal(file, &result)
+	assert.NilError(t, err, "error unmarshalling file")
+
+	index := 0
+	for i := range result.Results {
+		if strings.EqualFold(result.Results[i].Type, params.SastType) {
+			index = i
+			break
+		}
+	}
+
+	similarityID := result.Results[index].SimilarityID
+	scanType := result.Results[index].Type
+
+	severity := "HIGH"
+	comment := "Testing CLI Command with custom state-id."
+
+	args := []string{
+		"triage", "update",
+		flag(params.ProjectIDFlag), projectID,
+		flag(params.SimilarityIDFlag), similarityID,
+		flag(params.CustomStateIDFlag), "324",
+		flag(params.StateFlag), "triageTest",
+		flag(params.SeverityFlag), severity,
+		flag(params.CommentFlag), comment,
+		flag(params.ScanTypeFlag), scanType,
+	}
+
+	err, outputBufferForStep1 := executeCommand(t, args...)
+	_, readingError := io.ReadAll(outputBufferForStep1)
+	assert.NilError(t, readingError, "Reading result should pass")
+	assert.NilError(t, err, "Updating the predicate with custom state-id should pass.")
+
+	fmt.Println("Step 2: Testing the command 'triage show' to verify the updated predicate with state-id.")
+	outputBufferForStep2 := executeCmdNilAssertion(
+		t, "Predicates should be fetched.", "triage", "show",
+		flag(params.FormatFlag), printer.FormatJSON,
+		flag(params.ProjectIDFlag), projectID,
+		flag(params.SimilarityIDFlag), similarityID,
+		flag(params.ScanTypeFlag), scanType,
+	)
+
+	predicateResult := []wrappers.Predicate{}
+	_ = unmarshall(t, outputBufferForStep2, &predicateResult, "Reading results should pass")
+
+
+	assert.Assert(t, len(predicateResult) >= 1, "Should have at least 1 predicate as the result.")
+	found := false
+	for _, predicate := range predicateResult {
+		if predicate.StateID == 324 {
+			assert.Equal(t, predicate.State, "triageTest", "The state name for state-id 324 should be updated to triageTest")
+			found = true
+			break
+		}
+	}
+	assert.Assert(t, found, "Predicate with state-id 324 should be found in the results")
+}
