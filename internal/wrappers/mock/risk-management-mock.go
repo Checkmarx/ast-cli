f95b96cd6971503dc58056994088ec651c6eda74
galactica <tiago.baptista@checkmarx.com>
2025-03-12 21:58:17 +0200

    Add tests

---
+++ internal/wrappers/mock/risk-management-mock.go
@@ -0,0 +1,21 @@
+package mock
+
+import (
+	"github.com/checkmarx/ast-cli/internal/wrappers"
+)
+
+type RisksManagementMockWrapper struct{}
+
+func (r *RisksManagementMockWrapper) GetTopVulnerabilitiesByProjectID(projectID string) (*wrappers.ASPMResult, *wrappers.WebError, error) {
+	mockResults := []wrappers.RiskManagementResults{
+		{ID: "1", Name: "Vuln1", Severity: "High"},
+		{ID: "2", Name: "Vuln2", Severity: "Medium"},
+	}
+
+	mockASPMResult := &wrappers.ASPMResult{
+		ProjectID: projectID,
+		Results:   mockResults,
+	}
+
+	return mockASPMResult, nil, nil
+}
