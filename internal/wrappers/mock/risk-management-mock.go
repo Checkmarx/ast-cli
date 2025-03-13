95b2908682f8a3f254c755f497bc8fba44d2c42f
galactica <tiago.baptista@checkmarx.com>
2025-03-13 10:46:18 +0200

    Fix mock.RiskManagementMockWrapper name

--- internal/wrappers/mock/risk-management-mock.go
+++ internal/wrappers/mock/risk-management-mock.go
@@ -4,9 +4,9 @@ import (
 	"github.com/checkmarx/ast-cli/internal/wrappers"
 )
 
-type RisksManagementMockWrapper struct{}
+type RiskManagementMockWrapper struct{}
 
-func (r *RisksManagementMockWrapper) GetTopVulnerabilitiesByProjectID(projectID string) (*wrappers.ASPMResult, *wrappers.WebError, error) {
+func (r *RiskManagementMockWrapper) GetTopVulnerabilitiesByProjectID(projectID string) (*wrappers.ASPMResult, *wrappers.WebError, error) {
 	mockResults := []wrappers.RiskManagementResult{
 		{ID: "1", Name: "Vuln1", Severity: "High"},
 		{ID: "2", Name: "Vuln2", Severity: "Medium"},
