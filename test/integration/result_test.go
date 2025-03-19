b674350ea71b3e2e6adc60aeb0cfff2aaba004fd
galactica <tiago.baptista@checkmarx.com>
2025-03-19 12:56:17 +0200

    Do not skip on test.

--- test/integration/result_test.go
+++ test/integration/result_test.go
@@ -616,7 +616,6 @@ func readAndUnmarshalFile(t *testing.T, path string, v interface{}) {
 }
 
 func TestRiskManagementResults_ReturnResults(t *testing.T) {
-	t.Skip("Skipping this test")
 	projectName := "ast-phoenix-test-project"
 	_ = executeCmdNilAssertion(
 		t, "Create project should pass",
