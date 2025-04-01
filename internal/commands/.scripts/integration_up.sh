7f1dfada38dcdf7679718f6037f26aff6f2d114e
elchananarb <elchanan.arbiv@checkmarx.com>
2025-04-01 15:01:01 +0300

    Update integration_up.sh

--- internal/commands/.scripts/integration_up.sh
+++ internal/commands/.scripts/integration_up.sh
@@ -27,7 +27,7 @@ go test \
     -timeout 210m \
     -coverpkg github.com/checkmarx/ast-cli/internal/commands,github.com/checkmarx/ast-cli/internal/services,github.com/checkmarx/ast-cli/internal/wrappers \
     -coverprofile cover.out \
-    -run "^TestHooksPreCommitFullIntegration$" \
+    -run "^TestHooksPreCommit" \
     github.com/checkmarx/ast-cli/test/integration 2>&1 | tee test_output.log
 
 # Generate the initial HTML coverage report
