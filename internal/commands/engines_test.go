55a1256c6a41d40fe93e95a55e54dd77aa8f6c19
Sumit Morchhale <gurjarsumit7898@gmail.com>
2025-04-16 23:06:02 +0530

    code cleanup

--- internal/commands/engines_test.go
+++ internal/commands/engines_test.go
@@ -7,104 +7,78 @@ import (
 )
 
 func TestNewEnginesCommand(t *testing.T) {
-	//stateValues := "state=exclude_not_exploitable;TO_VERIFY;PROPOSED_NOT_EXPLOITABLE;CONFIRMED;URGENT"
-	//baseArgs := []string{"engines"}
 	cmd := NewEnginesCommand(&mock.NewHTTPEnginesMockWrapper{})
 	err := executeTestCommand(cmd, "")
 	assert.NilError(t, err)
 }
 
 func TestNewEnginesCommandHelp(t *testing.T) {
-	//stateValues := "state=exclude_not_exploitable;TO_VERIFY;PROPOSED_NOT_EXPLOITABLE;CONFIRMED;URGENT"
-	//baseArgs := []string{"engines"}
 	cmd := NewEnginesCommand(&mock.NewHTTPEnginesMockWrapper{})
 	err := executeTestCommand(cmd, "help")
 	assert.NilError(t, err)
 }
 
 func TestNewEnginesSubCommand(t *testing.T) {
-	//stateValues := "state=exclude_not_exploitable;TO_VERIFY;PROPOSED_NOT_EXPLOITABLE;CONFIRMED;URGENT"
-	//baseArgs := []string{"engines"}
 	cmd := NewEnginesCommand(&mock.NewHTTPEnginesMockWrapper{})
 	err := executeTestCommand(cmd, "list-api")
 	assert.NilError(t, err)
 }
 
 func TestNewEnginesSubCommandHelp(t *testing.T) {
-	//stateValues := "state=exclude_not_exploitable;TO_VERIFY;PROPOSED_NOT_EXPLOITABLE;CONFIRMED;URGENT"
-	//baseArgs := []string{"engines"}
 	cmd := NewEnginesCommand(&mock.NewHTTPEnginesMockWrapper{})
 	err := executeTestCommand(cmd, "list-api", "--help")
 	assert.NilError(t, err)
 }
 
 func TestSubCommandEngineType1(t *testing.T) {
-	//stateValues := "state=exclude_not_exploitable;TO_VERIFY;PROPOSED_NOT_EXPLOITABLE;CONFIRMED;URGENT"
-	//baseArgs := []string{"engines"}
 	cmd := NewEnginesCommand(&mock.NewHTTPEnginesMockWrapper{})
 	err := executeTestCommand(cmd, "list-api", "--engine-name", "SAST")
 	assert.NilError(t, err)
 }
 
 func TestSubCommandEngineType2(t *testing.T) {
-	//stateValues := "state=exclude_not_exploitable;TO_VERIFY;PROPOSED_NOT_EXPLOITABLE;CONFIRMED;URGENT"
-	//baseArgs := []string{"engines"}
 	cmd := NewEnginesCommand(&mock.NewHTTPEnginesMockWrapper{})
 	err := executeTestCommand(cmd, "list-api", "--engine-name", "SCA")
 	assert.NilError(t, err)
 }
 
 func TestSubCommandEngineType3(t *testing.T) {
-	//stateValues := "state=exclude_not_exploitable;TO_VERIFY;PROPOSED_NOT_EXPLOITABLE;CONFIRMED;URGENT"
-	//baseArgs := []string{"engines"}
 	cmd := NewEnginesCommand(&mock.NewHTTPEnginesMockWrapper{})
 	err := executeTestCommand(cmd, "list-api", "--engine-name", "Iac")
 	assert.NilError(t, err)
 }
 
 func TestSubCommandOutPutFormat1(t *testing.T) {
-	//stateValues := "state=exclude_not_exploitable;TO_VERIFY;PROPOSED_NOT_EXPLOITABLE;CONFIRMED;URGENT"
-	//baseArgs := []string{"engines"}
 	cmd := NewEnginesCommand(&mock.NewHTTPEnginesMockWrapper{})
 	err := executeTestCommand(cmd, "list-api", "--output-format", "json")
 	assert.NilError(t, err)
 }
 
 func TestSubCommandOutPutFormat2(t *testing.T) {
-	//stateValues := "state=exclude_not_exploitable;TO_VERIFY;PROPOSED_NOT_EXPLOITABLE;CONFIRMED;URGENT"
-	//baseArgs := []string{"engines"}
 	cmd := NewEnginesCommand(&mock.NewHTTPEnginesMockWrapper{})
 	err := executeTestCommand(cmd, "list-api", "--output-format", "yaml")
 	assert.NilError(t, err)
 }
 
 func TestSubCommandOutPutFormat3(t *testing.T) {
-	//stateValues := "state=exclude_not_exploitable;TO_VERIFY;PROPOSED_NOT_EXPLOITABLE;CONFIRMED;URGENT"
-	//baseArgs := []string{"engines"}
 	cmd := NewEnginesCommand(&mock.NewHTTPEnginesMockWrapper{})
 	err := executeTestCommand(cmd, "list-api", "--output-format", "table")
 	assert.NilError(t, err)
 }
 
 func TestSubCommandEngineError1(t *testing.T) {
-	//stateValues := "state=exclude_not_exploitable;TO_VERIFY;PROPOSED_NOT_EXPLOITABLE;CONFIRMED;URGENT"
-	//baseArgs := []string{"engines"}
 	cmd := NewEnginesCommand(&mock.NewHTTPEnginesMockWrapper{})
 	err := executeTestCommand(cmd, "list-api", "--chibute", "SAST")
 	assert.Assert(t, err != nil)
 }
 
 func TestSubCommandEngineError2(t *testing.T) {
-	//stateValues := "state=exclude_not_exploitable;TO_VERIFY;PROPOSED_NOT_EXPLOITABLE;CONFIRMED;URGENT"
-	//baseArgs := []string{"engines"}
 	cmd := NewEnginesCommand(&mock.NewHTTPEnginesMockWrapper{})
 	err := executeTestCommand(cmd, "list-api", "--engine-name", "SASTS")
 	assert.NilError(t, err)
 }
 
 func TestSubCommandEngineError3(t *testing.T) {
-	//stateValues := "state=exclude_not_exploitable;TO_VERIFY;PROPOSED_NOT_EXPLOITABLE;CONFIRMED;URGENT"
-	//baseArgs := []string{"engines"}
 	cmd := NewEnginesCommand(&mock.NewHTTPEnginesMockWrapper{})
 	err := executeTestCommand(cmd, "list-api", "--output-format", "jsonsa")
 	assert.Assert(t, err != nil)
