9af3e93a90d280fb70899b8671dcdd5d6b791816
Sumit Morchhale <gurjarsumit7898@gmail.com>
2025-04-23 13:15:11 +0530

    engines list-api feature added

---
+++ internal/wrappers/engines-http.go
@@ -0,0 +1,21 @@
+package wrappers
+
+type EnginesHTTPWrapper struct{}
+
+func NewHTTPEnginesWrapper() EnginesWrapper { return &EnginesHTTPWrapper{} }
+func (e *EnginesHTTPWrapper) GetAllAPIs(engineName string) ([]ApiModel, *ErrorModel, error) {
+	var filteredApiModels []ApiModel = FilterByEngineType(ApiModels, engineName)
+	return filteredApiModels, nil, nil
+}
+func FilterByEngineType(apiModel []ApiModel, engineName string) []ApiModel {
+	var filtered []ApiModel
+	if engineName == "" {
+		return apiModel
+	}
+	for _, apiM := range apiModel {
+		if apiM.EngineName == engineName {
+			filtered = append(filtered, apiM)
+		}
+	}
+	return filtered
+}
