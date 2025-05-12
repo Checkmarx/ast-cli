3f6f3a6b4aac89fc3bc21defe2623d13d6e5b5d1
cx-Margarita-LevitM <cx-margarita-levitm>
2025-05-12 10:08:17 +0300

    Update due to code review

--- internal/wrappers/client.go
+++ internal/wrappers/client.go
@@ -112,7 +112,7 @@ func retryHTTPForIAMRequest(requestFunc func() (*http.Response, error), retries 
 		}
 		if resp.StatusCode >= 500 && resp.StatusCode <= 504 {
 			logger.PrintIfVerbose(fmt.Sprintf("Encountered HTTP %s response — will retry ", resp.Status))
-		} else if resp.StatusCode == http.StatusInternalServerError {
+		} else if resp.StatusCode == http.StatusUnauthorized {
 			logger.PrintIfVerbose("Unauthorized request (401), refreshing token")
 			_, _ = configureClientCredentialsAndGetNewToken()
 		} else {
