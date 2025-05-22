84e09c594e40ca6858913d970fb00c437b5d1214
cx-ben-alvo <144705560+cx-ben-alvo@users.noreply.github.com>
2025-05-22 16:17:59 +0300

    Add vulnerability handling in OSS package and cache logic
    Introduced support for tracking vulnerabilities in OSS package data structures and cache handling. Updated relevant methods to process and populate vulnerability details from realtime scanner and cache. This change enhances reporting and visibility of security issues in OSS packages.

--- internal/services/ossrealtime/config.go
+++ internal/services/ossrealtime/config.go
@@ -1,5 +1,10 @@
 package ossrealtime
 
+import (
+	"github.com/checkmarx/ast-cli/internal/services/ossrealtime/osscache"
+	"github.com/checkmarx/ast-cli/internal/wrappers"
+)
+
 // OssPackage represents a package's details for OSS scanning.
 type OssPackage struct {
 	PackageManager  string          `json:"PackageManager"`
@@ -23,4 +28,28 @@ type Vulnerability struct {
 	CVE         string `json:"CVE"`
 	Description string `json:"Description"`
 	Severity    string `json:"Severity"`
+}
+
+func NewOssVulnerabilitiesFromRealtimeScannerVulnerabilities(vulnerabilities []wrappers.RealtimeScannerVulnerability) []Vulnerability {
+	vulns := make([]Vulnerability, len(vulnerabilities))
+	for i, v := range vulnerabilities {
+		vulns[i] = Vulnerability{
+			CVE:         v.CVE,
+			Description: v.Description,
+			Severity:    v.Severity,
+		}
+	}
+	return vulns
+}
+
+func NewOssVulnerabilitiesFromOssCacheVulnerabilities(vulnerabilities []osscache.Vulnerability) []Vulnerability {
+	vulns := make([]Vulnerability, len(vulnerabilities))
+	for i, v := range vulnerabilities {
+		vulns[i] = Vulnerability{
+			CVE:         v.CVE,
+			Description: v.Description,
+			Severity:    v.Severity,
+		}
+	}
+	return vulns
 }
