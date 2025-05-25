791cdc01e58946df80dfa45a1b129061030453fb
cx-ben-alvo <144705560+cx-ben-alvo@users.noreply.github.com>
2025-05-25 09:36:03 +0300

    Add test for OSS realtime scan with vulnerable packages and update package.json

--- internal/services/ossrealtime/osscache/types.go
+++ internal/services/ossrealtime/osscache/types.go
@@ -2,8 +2,6 @@ package osscache
 
 import (
 	"time"
-
-	"github.com/checkmarx/ast-cli/internal/wrappers"
 )
 
 type PackageEntry struct {
@@ -18,18 +16,6 @@ type Vulnerability struct {
 	CVE         string `json:"CVE"`
 	Description string `json:"Description"`
 	Severity    string `json:"Severity"`
-}
-
-func (p *PackageEntry) ConvertVulnerabilities() []wrappers.RealtimeScannerVulnerability {
-	vulnerabilities := make([]wrappers.RealtimeScannerVulnerability, len(p.Vulnerabilities))
-	for i, v := range p.Vulnerabilities {
-		vulnerabilities[i] = wrappers.RealtimeScannerVulnerability{
-			CVE:         v.CVE,
-			Description: v.Description,
-			Severity:    v.Severity,
-		}
-	}
-	return vulnerabilities
 }
 
 type Cache struct {
