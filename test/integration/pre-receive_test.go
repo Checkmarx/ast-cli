5e2b2cd66104e0536c89cc728112b37193eb8d20
anjali-deore <200181980+cx-anjali-deore@users.noreply.github.com>
2025-06-02 12:06:06 +0530

    Unit and integration tests

--- test/integration/pre-receive_test.go
+++ test/integration/pre-receive_test.go
@@ -3,20 +3,17 @@
 package integration
 
 import (
-	"fmt"
+	"github.com/stretchr/testify/assert"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func TestPreReceive_PushSecrets(t *testing.T) {
+func TestPreReceive_CommitPushSecrets(t *testing.T) {
 	workDir, cleanUp := setUpPreReceiveHookDir(t)
 	defer cleanUp()
 	assert.NoError(t, os.Chdir(workDir))
-	setGlobalGitAccount(t, workDir)
 
 	secretFile := filepath.Join(workDir, "secret.txt")
 	err := os.WriteFile(secretFile, []byte("ghp_DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD"), 0644)
@@ -46,14 +43,13 @@ func TestPreReceive_PushSecrets(t *testing.T) {
 	assert.Contains(t, outputString, "(pre-receive hook declined)")
 }
 
-func TestPreReceive_PushWithoutSecrets(t *testing.T) {
+func TestPreReceive_CommitPushWithoutSecrets(t *testing.T) {
 	workDir, cleanUp := setUpPreReceiveHookDir(t)
 	defer cleanUp()
 	assert.NoError(t, os.Chdir(workDir))
-	setGlobalGitAccount(t, workDir)
 
-	noSecretFile := filepath.Join(workDir, "without_secret.txt")
-	err := os.WriteFile(noSecretFile, []byte("Hello World"), 0644)
+	secretFile := filepath.Join(workDir, "without_secret.txt")
+	err := os.WriteFile(secretFile, []byte("Hello World"), 0644)
 	assert.NoError(t, err)
 	// Git add
 	outputCmd := exec.Command("git", "add", "without_secret.txt")
@@ -79,67 +75,18 @@ func TestPreReceive_PushWithoutSecrets(t *testing.T) {
 	assert.NotContains(t, outputString, "(pre-receive hook declined)")
 }
 
-func TestPreReceive_PushSecrets_and_NoSecretsFile(t *testing.T) {
-	workDir, cleanUp := setUpPreReceiveHookDir(t)
-	defer cleanUp()
-	assert.NoError(t, os.Chdir(workDir))
-	setGlobalGitAccount(t, workDir)
-	//create non-secret file
-	nonSecretFile := filepath.Join(workDir, "without_secret.txt")
-	err := os.WriteFile(nonSecretFile, []byte("Hello World"), 0644)
-	assert.NoError(t, err)
-	//create a secret file
-	secretFile := filepath.Join(workDir, "secret1.txt")
-	err = os.WriteFile(secretFile, []byte("ghp_DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD"), 0644)
-	assert.NoError(t, err)
-	// Git add
-	outputCmd := exec.Command("git", "add", "without_secret.txt", "secret1.txt")
-	// making it workingDir
-	outputCmd.Dir = workDir
-
-	output, err := outputCmd.CombinedOutput()
-	assert.NoError(t, err, "failed to add files in staging :%s", string(output))
-
-	// Add commit
-	commitCmd := exec.Command("git", "commit", "-m", "added without secrets file")
-	commitCmd.Dir = workDir
-	output, err = commitCmd.CombinedOutput()
-	assert.NoError(t, err, "Filed to commit :%s", string(output))
-	//Pushing
-	cmdPush := exec.Command("git", "push")
-	cmdPush.Dir = workDir
-	output, err = cmdPush.CombinedOutput()
-	outputString := string(output)
-	assert.Contains(t, outputString, "[remote rejected]")
-	assert.Contains(t, outputString, "(pre-receive hook declined)")
-	assert.Contains(t, outputString, "Detected 1 secret across 1 commit")
-}
-
-func setGlobalGitAccount(t *testing.T, repoName string) {
-	// Set global git config
-	githubUserName := os.Getenv("GITHUB_USER")
-	githubUserEmail := os.Getenv("GITHUB_EMAIL")
-	err := exec.Command("git", "-C", repoName, "config", "user.email", githubUserEmail).Run()
-	err = exec.Command("git", "-C", repoName, "config", "user.name", githubUserName).Run()
-	assert.NoError(t, err)
-}
-
 func setUpPreReceiveHookDir(t *testing.T) (workdir string, cleanup func()) {
 	orgWorkDir, err := os.Getwd()
 	assert.NoError(t, err)
 	tempDir := t.TempDir()
-	fmt.Println("the current dir" + orgWorkDir)
 
 	//Init a bare repo
 
 	err = exec.Command("git", "init", "--bare", filepath.Join(tempDir, "server")).Run()
 	assert.NoError(t, err)
-	cxPath := filepath.Join(orgWorkDir, "..", "..", "bin", "cx")
-	fmt.Println("the current dir" + cxPath)
-
 	preReceivePath := filepath.Join(tempDir, "server", "hooks", "pre-receive")
-	script := fmt.Sprintf(`#!/bin/bash
-							  "%s" hooks pre-receive secrets-scan`, cxPath)
+	script := `#!/bin/bash
+               cx hooks pre-receive secrets-scan`
 	err = os.WriteFile(preReceivePath, []byte(script), 0755)
 	assert.NoError(t, err)
 
