419c73a523ed9b9b29ef6d14684d1d256d99bf0c
Sumit Morchhale <gurjarsumit7898@gmail.com>
2025-06-02 17:08:07 +0530

    fix lint issue

--- internal/commands/scan.go
+++ internal/commands/scan.go
@@ -715,7 +715,7 @@ func scanCreateSubCommand(
 	createScanCmd.PersistentFlags().String(commonParams.ReportFormatJSONToEmailFlag, "", jsonToEmailFlagDescription)
 	createScanCmd.PersistentFlags().String(commonParams.ReportSbomFormatFlag, services.DefaultSbomOption, sbomReportFlagDescription)
 	createScanCmd.PersistentFlags().String(commonParams.ReportFormatPdfOptionsFlag, defaultPdfOptionsDataSections, pdfOptionsFlagDescription)
-	createScanCmd.PersistentFlags().String(commonParams.ReportFormatJSONOptionsFlag, defaultJsonOptionsDataSections, jsonOptionsFlagDescription)
+	createScanCmd.PersistentFlags().String(commonParams.ReportFormatJSONOptionsFlag, defaultJSONOptionsDataSections, jsonOptionsFlagDescription)
 	createScanCmd.PersistentFlags().String(commonParams.TargetFlag, "cx_result", "Output file")
 	createScanCmd.PersistentFlags().String(commonParams.TargetPathFlag, ".", "Output Path")
 	createScanCmd.PersistentFlags().StringSlice(commonParams.FilterFlag, []string{}, filterResultsListFlagUsage)
