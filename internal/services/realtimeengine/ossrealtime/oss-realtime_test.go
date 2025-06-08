12e194d5c82720f8d7b115b2037fbebd4506fc63
cx-ben-alvo <144705560+cx-ben-alvo@users.noreply.github.com>
2025-06-08 15:43:48 +0300

    Refactor file path constants and rename sensitive token variable

--- internal/services/realtimeengine/ossrealtime/oss-realtime_test.go
+++ internal/services/realtimeengine/ossrealtime/oss-realtime_test.go
@@ -44,7 +44,7 @@ func TestRunOssRealtimeScan_ValidLicenseAndManifest_ScanSuccess(t *testing.T) {
 		&mock.RealtimeScannerMockWrapper{},
 	)
 
-	const filePath = "../../commands/data/manifests/package.json"
+	const filePath = "../../../commands/data/manifests/package.json"
 
 	response, err := ossRealtimeService.RunOssRealtimeScan(filePath)
 
@@ -61,7 +61,7 @@ func TestRunOssRealtimeScan_InvalidLicenseAndValidManifest_ScanFail(t *testing.T
 		&mock.RealtimeScannerMockWrapper{},
 	)
 
-	const filePath = "../../commands/data/manifests/package.json"
+	const filePath = "../../../commands/data/manifests/package.json"
 
 	response, err := ossRealtimeService.RunOssRealtimeScan(filePath)
 
