6d225cd02f7f5d0f840a1ccb2f4cf28f4ce1586a
Sumit Morchhale <gurjarsumit7898@gmail.com>
2025-06-26 13:01:30 +0530

    Refactor the code

--- internal/commands/util/usercount/bitbucket.go
+++ internal/commands/util/usercount/bitbucket.go
@@ -97,7 +97,7 @@ func createRunBitBucketUserCountFunc(bitBucketWrapper wrappers.BitBucketWrapper)
 		err = printer.Print(cmd.OutOrStdout(), views, format)
 
 		// Only print user count information if in debug mode
-		if viper.GetBool(params.DebugFlag) || viper.GetString(params.LogFileFlag) != "" || viper.GetString(params.LogFileConsoleFlag) != "" {
+		if viper.GetBool(params.DebugFlag) || viper.GetString(params.LogFileFlag) != "" || viper.GetString(params.LogFileStdoutFlag) != "" {
 			err = printer.Print(cmd.OutOrStdout(), viewsUsers, format)
 		}
 
