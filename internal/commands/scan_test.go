bd7750a750ad3d6018e43594d048e67014d3f6a6
anjali-deore <200181980+cx-anjali-deore@users.noreply.github.com>
2025-07-03 20:05:06 +0530

    fixed space again

--- internal/commands/scan_test.go
+++ internal/commands/scan_test.go
@@ -622,8 +622,8 @@ func TestCreateScanWithPrimaryBranchFlagBooleanValueFalse_Passed(t *testing.T) {
 func TestCreateScanWithPrimaryBranchFlagStringValue_Should_Fail(t *testing.T) {
 	err := execCmdNotNilAssertion(t, "scan", "create", "--project-name", "MOCK", "-s", dummyRepo, "-b", "dummy_branch", "--debug", "--branch-primary=string")
 	assert.ErrorContains(t, err, "invalid argument \"string\"", err.Error())
-
 }
+
 func Test_parseThresholdSuccess(t *testing.T) {
 	want := make(map[string]int)
 	want["iac-security-low"] = 1
@@ -642,7 +642,6 @@ func Test_parseThresholdsSuccess(t *testing.T) {
 		t.Errorf("parseThreshold() = %v, want %v", got, want)
 	}
 }
-
 func Test_parseThresholdParseError(t *testing.T) {
 	want := make(map[string]int)
 	threshold := " KICS - LoW=error"
@@ -650,7 +649,6 @@ func Test_parseThresholdParseError(t *testing.T) {
 		t.Errorf("parseThreshold() = %v, want %v", got, want)
 	}
 }
-
 func TestCreateScanProjectTags(t *testing.T) {
 	execCmdNilAssertion(t, scanCommand, "create", "--project-name", "MOCK", "-s", dummyRepo, "-b", "dummy_branch",
 		"--project-tags", "test", "--debug")
