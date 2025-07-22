85527f1814e8b0f046356aea6c36d64ee84953a7
miryamfoiferCX <miryam.foifer@checkmarx.com>
2025-07-22 09:48:36 +0300

    support limited context of Helm

--- internal/services/realtimeengine/containersrealtime/containers-realtime.go
+++ internal/services/realtimeengine/containersrealtime/containers-realtime.go
@@ -105,7 +105,7 @@ func parseContainersFile(filePath string) ([]types.ImageModel, error) {
 		return nil, errors.Errorf("directory does not exist: %s", scanPath)
 	}
 
-	files, envVars, _, err := extractor.ExtractFiles(scanPath)
+	files, envVars, _, err := extractor.ExtractFiles(scanPath, false)
 	if err != nil {
 		return nil, errors.Wrap(err, "error extracting files")
 	}
@@ -199,11 +199,14 @@ func (c *ContainersRealtimeService) buildContainerImageResults(responseImages, i
 
 func mergeImagesToResults(listOfImages []wrappers.ContainerImageResponseItem, result ContainerImageResults, images *[]types.ImageModel, filePath string) ContainerImageResults {
 	for _, respImg := range listOfImages {
-		locations := getImageLocations(images, respImg.ImageName, respImg.ImageTag)
+		locations, specificFilePath := getImageLocations(images, respImg.ImageName, respImg.ImageTag)
+		if specificFilePath == "" {
+			specificFilePath = filePath
+		}
 		containerImage := ContainerImage{
 			ImageName:       respImg.ImageName,
 			ImageTag:        respImg.ImageTag,
-			FilePath:        filePath,
+			FilePath:        specificFilePath,
 			Locations:       locations,
 			Status:          respImg.Status,
 			Vulnerabilities: convertVulnerabilities(respImg.Vulnerabilities),
@@ -213,15 +216,19 @@ func mergeImagesToResults(listOfImages []wrappers.ContainerImageResponseItem, re
 	return result
 }
 
-func getImageLocations(images *[]types.ImageModel, imageName, imageTag string) []realtimeengine.Location {
+func getImageLocations(images *[]types.ImageModel, imageName, imageTag string) ([]realtimeengine.Location, string) {
 	for i, img := range *images {
 		if img.Name == imageName+":"+imageTag || img.Name == imageName+"@"+imageTag {
 			location := convertLocations(&img.ImageLocations)
+			filePath := ""
+			if len(img.ImageLocations) > 0 {
+				filePath = img.ImageLocations[0].Path
+			}
 			*images = append((*images)[:i], (*images)[i+1:]...)
-			return location
+			return location, filePath
 		}
 	}
-	return []realtimeengine.Location{}
+	return []realtimeengine.Location{}, ""
 }
 
 // splitToImageAndTag splits the image string into name and tag components.
