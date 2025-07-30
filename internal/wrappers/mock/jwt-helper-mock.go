7356f77702317dc4b0c8689efd68c771668d3773
Anjali Deore <200181980+cx-anjali-deore@users.noreply.github.com>
2025-07-30 20:59:28 +0530

    Pre receive hook feature for secret scan(AST-89345) (#1146)
    * Moved to common hooks place
    
    * removed common hooks code
    
    * Added pre receive commands
    
    * Adding the commit version of secret detection
    
    # Conflicts:
    #	go.mod
    
    # Conflicts:
    #	go.sum
    
    # Conflicts:
    #	go.sum
    
    * Added pre receive in Hooks doc
    
    * formatted
    
    * typo changes
    
    * formatted the file
    
    * Adding secret detection to depgaurd allow list
    
    * removed whitespace
    
    * formatted with gofmt
    
    * indentation
    
    * indentation
    
    * indentation
    
    * url and formatting fixes
    
    * indentation alignment
    
    * Unit and integration tests
    
    * lint issues fixed
    
    * formatted unit test file
    
    * setting global config git
    
    * changing cx path
    
    * adjusting cx path
    
    * check if cx path exist
    
    * check if cx path exist
    
    * added cxPath
    
    * Adding githubUser and email
    
    * renaming functions
    
    * url and formatting fixes
    
    * indentation alignment
    
    * Unit and integration tests
    
    * lint issues fixed
    
    * formatted unit test file
    
    * setting global config git
    
    * changing cx path
    
    * adjusting cx path
    
    * check if cx path exist
    
    * check if cx path exist
    
    * added cxPath
    
    * Adding githubUser and email
    
    * renaming functions
    
    * removed the git username
    
    * using github actor
    
    * go mod file updated
    
    * Added additional tests for coverage
    
    * secret detection version upgrade
    
    * updegraded the version
    
    * updegraded the version
    
    * updegraded the version
    
    * added latest version of secret detection 1.2.1
    
    * pre receive validate command
    
    * updated the versions
    
    * formatted files
    
    * formatted imports
    
    * reverted the changed manifest parser version back to main
    
    * changed the help text
    
    * removed usused go sum

--- internal/wrappers/mock/jwt-helper-mock.go
+++ internal/wrappers/mock/jwt-helper-mock.go
@@ -13,13 +13,15 @@ type JWTMockWrapper struct {
 
 const AIProtectionDisabled = 1
 
+var engines = []string{"sast", "sca", "api-security", "iac-security", "scs", "containers", "enterprise-secrets"}
+
 // GetAllowedEngines mock for tests
 func (j *JWTMockWrapper) GetAllowedEngines(featureFlagsWrapper wrappers.FeatureFlagsWrapper) (allowedEngines map[string]bool, err error) {
 	if j.CustomGetAllowedEngines != nil {
 		return j.CustomGetAllowedEngines(featureFlagsWrapper)
 	}
 	allowedEngines = make(map[string]bool)
-	engines := []string{"sast", "iac-security", "sca", "api-security", "containers", "scs"}
+
 	for _, value := range engines {
 		allowedEngines[strings.ToLower(value)] = true
 	}
