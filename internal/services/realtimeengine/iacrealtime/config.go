8c62c35ca346d215d8d15055677fb950686352ce
Ben Alvo <144705560+cx-ben-alvo@users.noreply.github.com>
2025-08-12 15:38:12 +0300

    Add remediation advice field to IacRealtimeResult (#1269)

--- internal/services/realtimeengine/iacrealtime/config.go
+++ internal/services/realtimeengine/iacrealtime/config.go
@@ -3,12 +3,13 @@ package iacrealtime
 import "github.com/checkmarx/ast-cli/internal/services/realtimeengine"
 
 type IacRealtimeResult struct {
-	SimilarityID string                    `json:"SimilarityID"`
-	Title        string                    `json:"Title"`
-	Description  string                    `json:"Description"`
-	Severity     string                    `json:"Severity"`
-	FilePath     string                    `json:"FilePath"`
-	Locations    []realtimeengine.Location `json:"Locations"`
+	SimilarityID      string                    `json:"SimilarityID"`
+	Title             string                    `json:"Title"`
+	Description       string                    `json:"Description"`
+	RemediationAdvice string                    `json:"RemediationAdvice"`
+	Severity          string                    `json:"Severity"`
+	FilePath          string                    `json:"FilePath"`
+	Locations         []realtimeengine.Location `json:"Locations"`
 }
 
 type IgnoredIacFinding struct {
