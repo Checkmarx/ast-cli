4fa5eabc9ade160a03b4f0c0ba868101322d0709
Dhanashree Wani <dhanashree.wani@checkmarx.com>
2025-10-07 11:23:54 +0530

    Added triage and results help

--- test/integration/help_test.go
+++ test/integration/help_test.go
@@ -14,6 +14,8 @@ import (
 // verify cx --help
 // verify cx project --help
 // verify cx scan --help
+// verify cx triage --help
+// verify cx results --help
 func TestCxRootHelpCommand(t *testing.T) {
 	// Capture the output of the help command
 	buffer := executeCmdNilAssertion(
@@ -49,7 +51,7 @@ func TestProjectHelpCommand(t *testing.T) {
 	output := string(result)
 	fmt.Println("Help project output:\n", output)
 
-	// Assert it contains some expected help output
+	// Assert it contains expected help output
 	assert.Assert(t, strings.Contains(output, "The project command enables the ability to manage projects in Checkmarx One"), "Help output should contain command description")
 	assert.Assert(t, strings.Contains(output, "cx project [flags]"), "Help output should contain usage information")
 	assert.Assert(t, strings.Contains(output, "COMMANDS"), "Help output should list available COMMANDS")
@@ -74,5 +76,49 @@ func TestScanHelpCommand(t *testing.T) {
 	// Assert it contains some expected help output
 	assert.Assert(t, strings.Contains(output, "The scan command enables the ability to manage scans in Checkmarx One"), "Help output should contain command description")
 	assert.Assert(t, strings.Contains(output, "cx scan [flags]"), "Help output should contain usage information")
+	assert.Assert(t, strings.Contains(output, "COMMANDS"), "Help output should list available COMMANDS")
+}
+
+func TestTriageHelpCommand(t *testing.T) {
+	// Capture the output of the help command
+	buffer := executeCmdNilAssertion(
+		t,
+		"The help command for 'triage' should execute successfully",
+		"triage", "--help",
+	)
+
+	// Read the output
+	result, err := io.ReadAll(buffer)
+	assert.NilError(t, err, "Reading help command output should succeed")
+
+	// Convert output to string and check for expected content
+	output := string(result)
+	fmt.Println("Help triage output:\n", output)
+
+	// Assert it contains expected help output
+	assert.Assert(t, strings.Contains(output, "The 'triage' command enables the ability to manage results in Checkmarx One"), "Help output should contain command description")
+	assert.Assert(t, strings.Contains(output, "cx triage [flags]"), "Help output should contain usage information")
+	assert.Assert(t, strings.Contains(output, "COMMANDS"), "Help output should list available COMMANDS")
+}
+
+func TestResultsHelpCommand(t *testing.T) {
+	// Capture the output of the help command
+	buffer := executeCmdNilAssertion(
+		t,
+		"The help command for 'results' should execute successfully",
+		"results", "--help",
+	)
+
+	// Read the output
+	result, err := io.ReadAll(buffer)
+	assert.NilError(t, err, "Reading help command output should succeed")
+
+	// Convert output to string and check for expected content
+	output := string(result)
+	fmt.Println("Help results output:\n", output)
+
+	// Assert it contains expected help output
+	assert.Assert(t, strings.Contains(output, "Retrieve results"), "Help output should contain command description")
+	assert.Assert(t, strings.Contains(output, "cx results [flags]"), "Help output should contain usage information")
 	assert.Assert(t, strings.Contains(output, "COMMANDS"), "Help output should list available COMMANDS")
 }
