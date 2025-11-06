e66c5573a88187f46112e8538ff700859b92cce1
Sumit Morchhale <sumit.morchhale@checkmarx.com>
2025-11-06 20:41:53 +0530

    debug unit test case

--- internal/commands/scan.go
+++ internal/commands/scan.go
@@ -2094,6 +2094,8 @@ func uploadZip(uploadsWrapper wrappers.UploadsWrapper, zipFilePath string, unzip
 	// check for INCREASE_FILE_UPLOAD_LIMIT feature flag
 	flagResponse, _ := wrappers.GetSpecificFeatureFlag(featureFlagsWrapper, wrappers.IncreaseFileUploadLimit)
 
+	fmt.Println("flagResponse.Status: ", flagResponse.Status)
+	fmt.Println("fileInfo.Size(): ", fileInfo.Size())
 	if flagResponse.Status && fileInfo.Size() > MaxSizeBytes {
 		// File size >5GB, proceed with multipart upload
 		logger.PrintIfVerbose("File size >5GB and INCREASE_FILE_UPLOAD_LIMIT flag is enabled,hence uploading file in multiple parts...")
