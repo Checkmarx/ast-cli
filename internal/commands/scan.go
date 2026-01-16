697fc1777d7c7c52b41822a5585dfc522d683a75
Bruno Silva <73999905+cx-bruno-silva@users.noreply.github.com>
2026-01-16 07:21:13 +0000

    Update kics version from 2.1.18 to 2.1.19 (AST-128989) (#1403)
    Co-authored-by: Anurag Dalke <anurag.dalke@checkmarx.com>

--- internal/commands/scan.go
+++ internal/commands/scan.go
@@ -62,7 +62,7 @@ const (
 	containerVolumeFlag                     = "-v"
 	containerNameFlag                       = "--name"
 	containerRemove                         = "--rm"
-	containerImage                          = "checkmarx/kics:v2.1.18"
+	containerImage                          = "checkmarx/kics:v2.1.19"
 	containerScan                           = "scan"
 	containerScanPathFlag                   = "-p"
 	containerScanPath                       = "/path"
