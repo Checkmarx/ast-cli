97643d2a78d3d5c4ce32e7967f90d8babd802322
anjali-deore <200181980+cx-anjali-deore@users.noreply.github.com>
2026-03-18 09:55:50 +0530

    - Initial Scan config triggered

--- internal/wrappers/feature-flags.go
+++ internal/wrappers/feature-flags.go
@@ -20,6 +20,8 @@ const DirectAssociationEnabled = "DIRECT_APP_ASSOCIATION_ENABLED"
 const maxRetries = 3
 const IncreaseFileUploadLimit = "INCREASE_FILE_UPLOAD_LIMIT"
 const ScaDeltaScanEnabled = "SCA_DELTASCAN_ENABLED"
+const AISupplyChainEnabled = "AI_SUPPLY_CHAIN_ENGINE_ENABLED"
+const AISupplyChainGAEnabled = "AI_SUPPLY_CHAIN_ENGINE_GA_ENABLED"
 
 var DefaultFFLoad bool = false
 
