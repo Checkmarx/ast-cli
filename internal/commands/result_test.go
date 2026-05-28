780b52e7adad38b45d78bef5dbedae9b1f662557
atishj99 <141334503+cx-atish-jadhav@users.noreply.github.com>
2026-05-28 20:48:05 +0530

    CVE-2026-33813: fixing cxone scan vulnerability

--- internal/commands/result_test.go
+++ internal/commands/result_test.go
@@ -1591,7 +1591,8 @@ func TestIgnorePolicyWithNoPermission(t *testing.T) {
 		t.Fatalf("failed to copy output: %v", err) // Handle the error if io.Copy fails
 	}
 	output := buf.String()
-	assert.Assert(t, strings.Contains(output, "Warning: The --ignore-policy flag was not implemented because you don’t have the required permission."), "'Ignore Policy flag omitted because you dont have permission' should not be present in the output")
+	expectedWarning := "Warning: The --ignore-policy flag was not implemented because you do not have the required permission."
+	assert.Assert(t, strings.Contains(output, expectedWarning), "’Ignore Policy flag omitted because you dont have permission’ should not be present in the output")
 }
 
 func TestIgnorePolicyWithPermission(t *testing.T) {
@@ -1625,7 +1626,8 @@ func TestIgnorePolicyWithPermission(t *testing.T) {
 		t.Fatalf("failed to copy output: %v", err) // Handle the error if io.Copy fails
 	}
 	output := buf.String()
-	assert.Assert(t, !strings.Contains(output, "Warning: The --ignore-policy flag was not implemented because you don’t have the required permission."), "'Ignore Policy flag omitted because you dont have permission' should not be present in the output")
+	expectedWarning := "Warning: The --ignore-policy flag was not implemented because you do not have the required permission."
+	assert.Assert(t, !strings.Contains(output, expectedWarning), "’Ignore Policy flag omitted because you dont have permission’ should not be present in the output")
 }
 
 func TestParseGlSastVulnerability_QueryDescriptionLink_Succeed(t *testing.T) {
